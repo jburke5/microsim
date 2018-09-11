615418d168c73805d17f4a43976b374d7e153e8a
burke <jburke5@gmail.com>
2018-09-11 07:54:20 -0400

    formatting

--- mcm/person.py
+++ mcm/person.py
@@ -1,14 +1,13 @@
 class Person:
     """Person is using risk factors and demographics based off NHANES"""
-    
+
     def __init__(self, age, gender, race_ethnicity, sbp, dbp, a1c, hdl, chol):
         self._gender = gender
         self._race_ethnicity = race_ethnicity
 
         self._age = [age]
         self._sbp = [sbp]
-        self._dbp  = [dbp]
+        self._dbp = [dbp]
         self._a1c = [a1c]
         self._hdl = [hdl]
         self._chol = [chol]
-
--- mcm/test/test_population.py
+++ mcm/test/test_population.py
@@ -8,17 +8,19 @@ class TestPopulation(unittest.TestCase):
     def setUp(self):
         self.test_n = 10000
         self.pandas_seed = 78483
-        test_nhanes =  pd.read_stata("mcm/nhanes2015-2016Combined.dta")
-        self.test_sample = test_nhanes.sample(self.test_n, weights=test_nhanes.wtint2yr, random_state=self.pandas_seed, replace=True)
-
-
+        test_nhanes = pd.read_stata("mcm/nhanes2015-2016Combined.dta")
+        self.test_sample = test_nhanes.sample(
+            self.test_n, weights=test_nhanes.wtint2yr, random_state=self.pandas_seed, replace=True)
 
     def test_people_from_population(self):
-        test_population = NHANESDirectSamplePopulation(self.test_n, self.pandas_seed)
+        test_population = NHANESDirectSamplePopulation(
+            self.test_n, self.pandas_seed)
         test_people = test_population._people
 
         test_ages = [x._age[0] for x in test_people]
-        self.assertAlmostEqual(np.mean(test_ages), self.test_sample.age.mean(), delta=0.000001)
+        self.assertAlmostEqual(
+            np.mean(test_ages), self.test_sample.age.mean(), delta=0.000001)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
