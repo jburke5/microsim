6faa1ae192c0bd5724fcaf8ea7e0ccc56a50d47e
burke <jburke5@gmail.com>
2018-09-11 07:54:20 -0400

    formatting

--- mcm/population.py
+++ mcm/population.py
@@ -2,15 +2,19 @@ from mcm.person import Person
 import pandas as pd
 import os
 
+
 class Population:
     """Unit of people subject to treatment program over time."""
 
     def __init__(self, people):
         self._people = people
 
-def build_people_using_nhanes_for_sampling(nhanes, n, random_seed = None):
-    repeated_sample = nhanes.sample(n, weights=nhanes.wtint2yr, random_state=random_seed, replace=True)
-    people = repeated_sample.apply(lambda x : Person(x.age, x.gender, x.raceEthnicity, x.meanSBP, x.meanDBP, x.a1c, x.hdl, x.chol), axis=1)
+
+def build_people_using_nhanes_for_sampling(nhanes, n, random_seed=None):
+    repeated_sample = nhanes.sample(
+        n, weights=nhanes.wtint2yr, random_state=random_seed, replace=True)
+    people = repeated_sample.apply(lambda x: Person(
+        x.age, x.gender, x.raceEthnicity, x.meanSBP, x.meanDBP, x.a1c, x.hdl, x.chol), axis=1)
     return people
 
 
@@ -19,6 +23,6 @@ class NHANESDirectSamplePopulation(Population):
 
     def __init__(self, n=10000, random_seed=None):
         nhanes = pd.read_stata("mcm/nhanes2015-2016Combined.dta")
-        super().__init__(build_people_using_nhanes_for_sampling(nhanes, n, random_seed=random_seed))
+        super().__init__(build_people_using_nhanes_for_sampling(
+            nhanes, n, random_seed=random_seed))
         self.n = n
-
