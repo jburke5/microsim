ada6316e1aba7f8fa5d3df1ac07128a1018c3182
burke <jburke5@gmail.com>
2018-10-13 08:08:21 -0400

    added bmi, smoking status

--- mcm/person.py
+++ mcm/person.py
@@ -5,7 +5,13 @@ class Person:
 
     # TODO: please remember to come back and make race and gender enums
     # TODO: probably should also add a view of the "most recent" version of risk factor values
-    def __init__(self, age, gender, race_ethnicity, sbp, dbp, a1c, hdl, tot_chol, **kwargs):
+    def __init__(self, age, gender, race_ethnicity, sbp, dbp, a1c, hdl, tot_chol, bmi,
+                 smoking_status, **kwargs):
+
+        # building in manual bounds on extreme values
+        self._lowerBounds = {"sbp": 50}
+        self._upperBounds = {"sbp": 300}
+
         self._gender = gender
         self._race_ethnicity = race_ethnicity
 
@@ -17,6 +23,9 @@ class Person:
         self._a1c = [a1c]
         self._hdl = [hdl]
         self._tot_chol = [tot_chol]
+        self._bmi = [bmi]
+        # TODO : change smoking status into a factor that changes over time
+        self._smoking_status = smoking_status
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -24,16 +33,29 @@ class Person:
         model = risk_model_repository.get(riskFactor)
         return model.estimate_next_risk(self._age[-1], self._gender, self._race_ethnicity,
                                         self._sbp[-1], self._dbp[-1], self._a1c[-1], self._hdl[-1],
-                                        self._tot_chol[-1])
+                                        self._tot_chol[-1], self._bmi[-1], self._smoking_status)
+
+    def apply_bounds(self, varName, varValue):
+        if varName in self._upperBounds:
+            upperBound = self._upperBounds[varName]
+            varValue = varValue if varValue < upperBound else upperBound
+        if varName in self._lowerBounds:
+            lowerBound = self._lowerBounds[varName]
+            varValue = varValue if varValue > lowerBound else lowerBound
+        return varValue
 
     def advance_risk_factors(self, risk_model_repository):
         ''' dummy risk models for now — 90% of your prior risk + a random normal intercept
         with a mean slightly greater than 10% of the population mean '''
-        self._sbp.append(self.get_next_risk_factor("sbp", risk_model_repository))
+        sbp = self.get_next_risk_factor("sbp", risk_model_repository)
+        sbp = self.apply_bounds("sbp", sbp)
+        self._sbp.append(sbp)
+
         self._dbp.append(self.get_next_risk_factor("dbp", risk_model_repository))
         self._a1c.append(self.get_next_risk_factor("a1c", risk_model_repository))
         self._hdl.append(self.get_next_risk_factor("hdl", risk_model_repository))
         self._tot_chol.append(self.get_next_risk_factor("tot_chol", risk_model_repository))
+        self._bmi.append(self.get_next_risk_factor("bmi", risk_model_repository))
         self._age.append(self._age[-1]+1)
 
     def advance_outcomes(self):
@@ -48,4 +70,6 @@ class Person:
                 f"a1c={self._a1c[-1]}, "
                 f"hdl={self._hdl[-1]}, "
                 f"tot_chol={self._tot_chol[-1]}"
+                f"bmi={self.bmi[-1]}"
+                f"smoking={self.smoking_status}"
                 f")")
