185d09a8116a125973ee8bf594add04da6e19b1a
burke <jburke5@gmail.com>
2018-10-19 08:45:22 -0400

    add NHANESRaceEthnicity enum

---
+++ mcm/race_ethnicity.py
@@ -0,0 +1,14 @@
+from enum import IntEnum
+
+
+class NHANESRaceEthnicity(IntEnum):
+    """
+    NHANES Race Ethinity enumeration.
+
+    Follows RIDRETH1.
+    """
+    MEXICAN_AMERICAN = 1
+    OTHER_HISPANIC = 2
+    NON_HISPANIC_WHITE = 3
+    NON_HISPANIC_BLACK = 4
+    OTHER = 5
