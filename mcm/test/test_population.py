dd91d9381c1ceab55d0e43b6281318dc11c5ce8d
burke <jburke5@gmail.com>
2019-02-13 07:58:20 -0500

    Added test to make sure that population accounts for people that die

--- mcm/test/test_population.py
+++ mcm/test/test_population.py
@@ -1,4 +1,10 @@
 from mcm.population import NHANESDirectSamplePopulation
+from mcm.population import Population
+from mcm.person import Person
+from mcm.gender import NHANESGender
+from mcm.race_ethnicity import NHANESRaceEthnicity
+from mcm.smoking_status import SmokingStatus
+
 import unittest
 import pandas as pd
 import numpy as np
@@ -21,6 +27,34 @@ class TestPopulation(unittest.TestCase):
         test_ages = [x._age[0] for x in test_people]
         self.assertAlmostEqual(
             np.mean(test_ages), self.test_sample.age.mean(), delta=0.000001)
+
+
+class TestPopulationAdvanceOutcomes(unittest.TestCase):
+    def setUp(self):
+        self.joe = Person(
+            42,
+            NHANESGender.MALE,
+            NHANESRaceEthnicity.NON_HISPANIC_BLACK,
+            140,
+            90,
+            5.5,
+            50,
+            200,
+            25,
+            90,
+            150,
+            SmokingStatus.NEVER)
+
+    def test_dont_advance_dead_people_in_population(self):
+        self.dummy_population = Population([self.joe])
+        self.joe._alive.append(False)
+        expected_risk_factor_length = len(self.joe._sbp)
+
+        # this should NOT raise an error if it is (correctly) not trying to
+        #  advance on poor dead, joe
+        self.dummy_population.advance(1)
+
+        self.assertEqual(expected_risk_factor_length, len(self.joe._sbp))
 
 
 if __name__ == "__main__":
