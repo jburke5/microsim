4d6b51d55c9a2e0af24fbb90e01a998accc16104
burke <jburke5@gmail.com>
2019-07-17 08:56:39 -0400

    Mmerge branch 'data-load-refactor'
    Squashed commit of the following:
    
    commit ce384a5452c9c4fc66b730870ea964f8a9999480
    Author: burke <jburke5@gmail.com>
    Date:   Wed Jul 17 08:55:03 2019 -0400
    
        fix bugs
    
    commit 9f05b022ceb281a39d300d2212c40710bcfc6307
    Author: burke <jburke5@gmail.com>
    Date:   Wed Jul 17 08:53:55 2019 -0400
    
        use data_loader
    
    commit f40e459022d5b38e8b8a1bba75fb1709bb749739
    Author: burke <jburke5@gmail.com>
    Date:   Wed Jul 17 08:53:25 2019 -0400
    
        add separate data filepath method
    
    commit 3f69fb796c9861546364158d0df4c179c76d4107
    Author: burke <jburke5@gmail.com>
    Date:   Wed Jul 17 08:43:58 2019 -0400
    
        add data_loader

--- mcm/cohort_risk_model_repository.py
+++ mcm/cohort_risk_model_repository.py
@@ -1,8 +1,8 @@
-from mcm.regression_model import RegressionModel
 from mcm.risk_model_repository import RiskModelRepository
 from mcm.statsmodel_linear_risk_factor_model import StatsModelLinearRiskFactorModel
 from mcm.stats_model_linear_probability_risk_factor_model import StatsModelLinearProbabilityRiskFactorModel
 from mcm.stats_model_rounded_linear_risk_factor_model import StatsModelRoundedLinearRiskFactorModel
+from mcm.data_loader import load_regression_model
 
 import json
 import os
@@ -25,23 +25,14 @@ class CohortRiskModelRepository(RiskModelRepository):
         self._initialize_linear_risk_model("sbp", "logSbpCohortModel", log=True)
         self._initialize_linear_risk_model("dbp", "logDbpCohortModel", log=True)
 
-    def _load_model_spec(self, referenceName, modelName):
-        abs_module_path = os.path.abspath(os.path.dirname(__file__))
-        model_spec_path = os.path.normpath(os.path.join(abs_module_path, "./data/",
-                                                        modelName + "Spec.json"))
-        with open(model_spec_path, 'r') as model_spec_file:
-            model_spec = json.load(model_spec_file)
-        return RegressionModel(**model_spec)
-
     def _initialize_linear_risk_model(self, referenceName, modelName, log=False):
-        model = self._load_model_spec(referenceName, modelName)
+        model = load_regression_model(modelName)
         self._repository[referenceName] = StatsModelLinearRiskFactorModel(model, log)
 
     def _initialize_linear_probability_risk_model(self, referenceName, modelName):
-        model = self._load_model_spec(referenceName, modelName)
+        model = load_regression_model(modelName)
         self._repository[referenceName] = StatsModelLinearProbabilityRiskFactorModel(model)
 
     def _initialize_int_rounded_linear_risk_model(self, referenceName, modelName):
-        model = self._load_model_spec(referenceName, modelName)
+        model = load_regression_model(modelName)
         self._repository[referenceName] = StatsModelRoundedLinearRiskFactorModel(model)
-
