7e6059fb3f820ff9081d6845f5f7dffd617fab13
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2019-12-07 15:37:02 -0500

    consolidate Cox model constructor

--- mcm/statsmodel_cox_model.py
+++ mcm/statsmodel_cox_model.py
@@ -5,15 +5,9 @@ import numpy as np
 class StatsModelCoxModel(StatsModelLinearRiskFactorModel):
     def __init__(self, regression_model, log_transform=False):
         super(StatsModelCoxModel, self).__init__(regression_model, log_transform)
-
-    # called by superclass...
-    def initialize_model_params(self, regression_model, log_transform):
-        self.parameters = regression_model._coefficients
-        self.standard_errors = regression_model._coefficient_standard_errors
         self.one_year_linear_cumulative_hazard = \
             regression_model._one_year_linear_cumulative_hazard
         self.one_year_quad_cumulative_hazard = regression_model._one_year_quad_cumulative_hazard
-        self.log_transform = log_transform
 
     def get_intercept(self):
         return 0
