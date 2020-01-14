908a8cc3b617aee162dbe911872210c73bff46e4
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2020-01-13 21:41:57 -0500

    add iterative impl of get_argument_transforms

--- mcm/statsmodel_linear_risk_factor_model.py
+++ mcm/statsmodel_linear_risk_factor_model.py
@@ -10,6 +10,56 @@ categorical_param_name_pattern = r"^(?P<propname>[^\[]+)\[T\.(?P<matchingval>[^\
 categorical_param_name_regex = re.compile(categorical_param_name_pattern)
 
 
+def get_argument_transforms(
+    parameter_name: str,
+    max_num_transforms: int = 10
+) -> Tuple[str, List[Callable]]:
+    trimmed_param_name = parameter_name
+    prop_transforms = []
+    reached_base_case = False
+    for _ in range(max_num_transforms):
+        folded_param_name = trimmed_param_name.casefold()
+        categorical_match = categorical_param_name_regex.match(parameter_name)
+        if categorical_match is not None:
+            expected_prop_name = categorical_match['propname']
+            matching_categorical_value = int(categorical_match['matchingval'])
+            prop_transforms.append(lambda v: 1 if v == matching_categorical_value else 0)
+            reached_base_case = True
+            break
+        elif folded_param_name.startswith("log"):
+            trimmed_param_name = trimmed_param_name[len("log"):]
+            prop_transforms.append(lambda v: np.log(v))
+        elif folded_param_name.startswith("mean"):
+            trimmed_param_name = trimmed_param_name[len("mean"):]
+            prop_transforms.append(lambda v: np.array(v).mean())
+        elif folded_param_name.startswith("square"):
+            trimmed_param_name = trimmed_param_name[len("square"):]
+            prop_transforms.append(lambda v: v ** 2)
+        elif folded_param_name.startswith("base"):
+            trimmed_param_name = trimmed_param_name[len("base"):]
+            prop_transforms.append(lambda v: v[0])
+        elif folded_param_name.startswith("lag"):
+            trimmed_param_name = trimmed_param_name[len("lag"):]
+            expected_prop_name = trimmed_param_name[0].casefold() + trimmed_param_name[1:]
+            # no transform: identity
+            reached_base_case = True
+            break
+        else:
+            expected_prop_name = trimmed_param_name[0].casefold() + trimmed_param_name[1:]
+            # no transform: identity
+            reached_base_case = True
+            break
+    if not reached_base_case:
+        raise RuntimeError(
+            f"Error occurred while parsing parameter name ('{parameter_name}') for model argument"
+            f" transforms: exceeded maximum number of iterations ({max_num_transforms}) without"
+            " reaching a base case.",
+            parameter_name,
+            max_num_transforms,
+        )
+    return (expected_prop_name, reversed(prop_transforms))
+
+
 def get_argument_transforms_recursive(parameter_name: str) -> Tuple[str, List[Callable]]:
     folded_param_name = parameter_name.casefold()
     categorical_match = categorical_param_name_regex.match(parameter_name)
