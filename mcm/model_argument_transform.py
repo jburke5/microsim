01cea275787be9af16e20f32fd2aefc803e90393
llc2 <45376498+llc2@users.noreply.github.com>
2020-01-14 08:06:35 -0500

    Add model argument transforms (#6)
    * remove unused initialze method
    
    * comment tweaks
    
    * cache model argument transformations
    
    * make residuals options for linear risk factor models
    
    * consolidate Cox model constructor
    
    * casefold only first character of prop name
    
    Person property names like 'totChol' exist. I don't think this is safe
    for all codepoints supported by Unicode, but I hope to improve upon it
    in future iterations.
    
    * add param to transforms dict even if empty
    
    somtimes property names differ from param name (e.g., lagSbp). (This
    appears to have been a bug from the original code, but will leave it in
    for now and as Jim.)
    
    * remove unused function
    
    * sort categorical params in model constructor
    
    * add categorical transforms
    
    * use categorical tranforms in model eval
    
    * remove unused function
    
    * append 'recursive' to 'get_argument_transforms'
    
    * add iterative impl of get_argument_transforms
    
    * use iterative impl. of get_argument_transforms
    
    tests seem to be passing! Not sure if it's cleaner, but it's probably
    the better than the recursive implementation for broader use.
    
    * add transform callables
    
    * formatting
    
    * more formatting
    
    * move model arg transforms to own file
    
    * remove recursive impl.
    
    * move get_argument_transforms to transforms file

---
+++ mcm/model_argument_transform.py
@@ -0,0 +1,112 @@
+from copy import copy
+from typing import Callable, Dict, Iterable, List, Tuple
+import re
+
+import numpy as np
+
+
+categorical_param_name_pattern = r"^(?P<propname>[^\[]+)\[T\.(?P<matchingval>[^\]]+)\]"
+categorical_param_name_regex = re.compile(categorical_param_name_pattern)
+
+
+class IndicatorTransform:
+    """
+    Transform that returns 1 if value matches; else 0.
+
+    matching_value should be either a primitive or a `copy`-able container of primitives.
+    """
+
+    def __init__(self, matching_value):
+        self._matching_value = copy(matching_value)
+
+    @property
+    def matching_value(self):
+        return self._matching_value
+
+    def __call__(self, value):
+        return 1 if value == self._matching_value else 0
+
+    def __eq__(self, other):
+        if issubclass(type(other), IndicatorTransform):
+            return self._matching_value == other.matching_value
+        return False
+
+
+def log_transform(value):
+    """Returns the log (one or many) of the given value."""
+    return np.log(value)
+
+
+def mean_transform(value):
+    """Returns the mean of the given value."""
+    return np.array(value).mean()
+
+
+def square_transform(value):
+    """Returns the square (one or many) of the given value."""
+    return value ** 2
+
+
+def base_transform(value):
+    """Returns the base (i.e., first) element of the given value."""
+    return value[0]
+
+
+def get_argument_transforms(
+    parameter_name: str,
+    max_num_transforms: int = 10,
+) -> Tuple[str, List[Callable]]:
+    trimmed_param_name = parameter_name
+    prop_transforms = []
+    reached_base_case = False
+    for _ in range(max_num_transforms):
+        folded_param_name = trimmed_param_name.casefold()
+        categorical_match = categorical_param_name_regex.match(parameter_name)
+        if categorical_match is not None:
+            expected_prop_name = categorical_match['propname']
+            matching_categorical_value = int(categorical_match['matchingval'])
+            prop_transforms.append(IndicatorTransform(matching_categorical_value))
+            reached_base_case = True
+            break
+        elif folded_param_name.startswith("log"):
+            trimmed_param_name = trimmed_param_name[len("log"):]
+            prop_transforms.append(log_transform)
+        elif folded_param_name.startswith("mean"):
+            trimmed_param_name = trimmed_param_name[len("mean"):]
+            prop_transforms.append(mean_transform)
+        elif folded_param_name.startswith("square"):
+            trimmed_param_name = trimmed_param_name[len("square"):]
+            prop_transforms.append(square_transform)
+        elif folded_param_name.startswith("base"):
+            trimmed_param_name = trimmed_param_name[len("base"):]
+            prop_transforms.append(base_transform)
+        elif folded_param_name.startswith("lag"):
+            trimmed_param_name = trimmed_param_name[len("lag"):]
+            expected_prop_name = trimmed_param_name[0].casefold() + trimmed_param_name[1:]
+            # no transform: identity
+            reached_base_case = True
+            break
+        else:
+            expected_prop_name = trimmed_param_name[0].casefold() + trimmed_param_name[1:]
+            # no transform: identity
+            reached_base_case = True
+            break
+    if not reached_base_case:
+        raise RuntimeError(
+            f"Error occurred while parsing parameter name ('{parameter_name}') for model argument"
+            f" transforms: exceeded maximum number of iterations ({max_num_transforms}) without"
+            " reaching a base case.",
+            parameter_name,
+            max_num_transforms,
+        )
+    return (expected_prop_name, reversed(prop_transforms))
+
+
+def get_all_argument_transforms(parameter_names: Iterable[str]) -> Dict[str, List[Callable]]:
+    """Return transform functions for each model parameter, if any"""
+    param_transforms = {}
+    for param_name in parameter_names:
+        prop_name, transforms = get_argument_transforms(param_name)
+        if param_name.casefold() != prop_name:
+            param_transforms[param_name] = (prop_name, transforms)
+    return param_transforms
--- mcm/statsmodel_linear_risk_factor_model.py
+++ mcm/statsmodel_linear_risk_factor_model.py
@@ -1,72 +1,34 @@
-
+from functools import reduce
 import numpy as np
+from mcm.model_argument_transform import get_all_argument_transforms
 
 # TODO: this class needs to be renamed. its no longer interfacing with statsmodel
 # conceptually, what it does now is bridge the regression model and the person
 
 
 class StatsModelLinearRiskFactorModel:
-
     def __init__(self, regression_model, log_transform=False):
-        self.initialize_model_params(regression_model, log_transform)
-
-    def initialize_model_params(self, regression_model, log_transform):
-        self.parameters = regression_model._coefficients
         self.standard_errors = regression_model._coefficient_standard_errors
-        self.residual_mean = regression_model._residual_mean
-        self.residual_standard_deviation = regression_model._residual_standard_deviation
         self.log_transform = log_transform
+        if (
+            hasattr(regression_model, "_residual_mean")
+            and hasattr(regression_model, "_residual_standard_deviation")
+        ):
+            self.residual_mean = regression_model._residual_mean
+            self.residual_standard_deviation = regression_model._residual_standard_deviation
+
+        self.parameters = {**(regression_model._coefficients)}
+        self.non_intercept_params = {k: v for k, v in self.parameters.items() if k != 'Intercept'}
+        self.argument_transforms = get_all_argument_transforms(self.non_intercept_params.keys())
 
     def draw_from_residual_distribution(self):
+        if not hasattr(self, "residual_mean") and hasattr(self, "residual_standard_deviation"):
+            raise RuntimeError("Cannot draw from residual distribution: model does not have"
+                               " residual information")
         return np.random.normal(
             loc=self.residual_mean,
             scale=self.residual_standard_deviation,
             size=1)[0]
-
-    def get_modified_attribute_for_parameter_from_person(self, name, person):
-        returnParam = self.get_modified_parameter_for_person(name, person)
-        if not isinstance(returnParam, list) and not isinstance(returnParam, np.ndarray):
-            return returnParam
-        else:
-            return returnParam[-1]
-
-    def convert_first_letter_to_lower(self, toLower):
-        return toLower[:1].lower() + toLower[1:]
-
-    '''
-    This will apply an order of operations to the elements of the mdoel name. So, meanLogLagSbp
-    would take lagSBP, log it and take the mean. If the order of operations for the first elements
-    matters,they'll be appleid in order of which they are listed...
-    '''
-
-    # luciana tag...
-    # profiling wise...this is where we're getting killed. 
-    # the solution seems to be to cache this logic in some way. the first time through, figure out the series of manimpuations, subsequently
-    # apply those manipulations — i just dont' see an easy way to do that. it can clearly be done...but, tis going to make my head bleed.
-    def get_modified_parameter_for_person(self, name, person):
-        if name.startswith("log"):
-            name = self.convert_first_letter_to_lower(name[len("log"):])
-            return np.log(self.get_modified_parameter_for_person(name, person))
-        elif name.startswith("mean"):
-            name = self.convert_first_letter_to_lower(name[len("mean"):])
-            return np.array(self.get_modified_parameter_for_person(name, person)).mean()
-        elif name.startswith("square"):
-            name = self.convert_first_letter_to_lower(name[len("square"):])
-            return (self.get_modified_parameter_for_person(name, person))**2
-        elif name.startswith("base"):
-            name = self.convert_first_letter_to_lower(name[len("base"):])
-            return getattr(person, "_" + name)[0]
-        # just strip lag prefixes
-        elif name.startswith("lag"):
-            name = self.convert_first_letter_to_lower(name[len("lag"):])
-            return getattr(person, "_" + name)
-        else:
-            return getattr(person, "_" + name)
-
-    def strip_categorical_name(self, name):
-        stripped_name = "_" + name[:name.index("[")]
-        stripped_value = int(name[name.index("[T.") + len("[T."): name.index("]")])
-        return (stripped_name, stripped_value)
 
     def get_intercept(self):
         return self.parameters['Intercept']
@@ -74,30 +36,17 @@ class StatsModelLinearRiskFactorModel:
     def estimate_next_risk(self, person):
         # TODO: think about what to do with teh hard-coded strings for parameters and prefixes
         linearPredictor = self.get_intercept()
-        nonInterceptParams = dict(self.parameters)
-        if 'Intercept' in nonInterceptParams.keys():
-            del nonInterceptParams['Intercept']
 
-        # sort parametesr into categorical and non-categorial
-        categoricalParams = {}
-        nonCategoricalParams = {}
-
-        for coeff_name, coeff_val in nonInterceptParams.items():
-            if "[" in coeff_name:
-                categoricalParams[coeff_name] = coeff_val
+        for coeff_name, coeff_val in self.non_intercept_params.items():
+            if coeff_name not in self.argument_transforms:
+                model_argument = getattr(person, f"_{coeff_name}")
             else:
-                nonCategoricalParams[coeff_name] = coeff_val
-
-        # for non-categorical parameters this is easy — just add the linear predictor
-        for coeff_name, coeff_val in nonCategoricalParams.items():
-            linearPredictor += coeff_val * \
-                self.get_modified_attribute_for_parameter_from_person(coeff_name, person)
-
-        # for categorical params, pick which parameter to add...
-        for coeff_name, coeff_val in categoricalParams.items():
-            stripped_name, matched_categorical_value = self.strip_categorical_name(coeff_name)
-            if (matched_categorical_value == getattr(person, stripped_name)):
-                linearPredictor += coeff_val
+                prop_name, transforms = self.argument_transforms[coeff_name]
+                prop_value = getattr(person, f"_{prop_name}")
+                model_argument = reduce(lambda v, f: f(v), transforms, prop_value)
+            if isinstance(model_argument, list) or isinstance(model_argument, np.ndarray):
+                model_argument = model_argument[-1]
+            linearPredictor += coeff_val * model_argument
 
         if (self.log_transform):
             linearPredictor = np.exp(linearPredictor)
