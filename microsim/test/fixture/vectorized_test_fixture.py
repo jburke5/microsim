ef62d616465c760ee72a4a35417793e5d209a261
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-03-17 19:15:46 -0400

    compute initial gcp for vec test person

--- microsim/test/fixture/vectorized_test_fixture.py
+++ microsim/test/fixture/vectorized_test_fixture.py
@@ -1,5 +1,6 @@
 import unittest
 
+from microsim.gcp_model import GCPModel
 from microsim.alcohol_category import AlcoholCategory
 from microsim.education import Education
 from microsim.gender import NHANESGender
@@ -19,31 +20,33 @@ class VectorizedTestFixture(unittest.TestCase):
     @classmethod
     def get_or_init_population_dataframe(cls):
         if VectorizedTestFixture._population_dataframe is None:
-            people = [
-                Person(
-                    age=71,
-                    gender=NHANESGender.MALE,
-                    raceEthnicity=NHANESRaceEthnicity.NON_HISPANIC_WHITE,
-                    sbp=144.667,
-                    dbp=52.6667,
-                    a1c=9.5,
-                    hdl=34,
-                    totChol=191,
-                    bmi=30.05,
-                    ldl=110.0,
-                    trig=128,
-                    waist=45,
-                    anyPhysicalActivity=0,
-                    education=Education.COLLEGEGRADUATE,
-                    smokingStatus=SmokingStatus.FORMER,
-                    alcohol=AlcoholCategory.NONE,
-                    antiHypertensiveCount=0,
-                    statin=0,
-                    otherLipidLoweringMedicationCount=0,
-                    initializeAfib=(lambda _: None),
-                    randomEffects={'gcp': 0},
-                ),
-            ]
+            test_person = Person(
+                age=71,
+                gender=NHANESGender.MALE,
+                raceEthnicity=NHANESRaceEthnicity.NON_HISPANIC_WHITE,
+                sbp=144.667,
+                dbp=52.6667,
+                a1c=9.5,
+                hdl=34,
+                totChol=191,
+                bmi=30.05,
+                ldl=110.0,
+                trig=128,
+                waist=45,
+                anyPhysicalActivity=0,
+                education=Education.COLLEGEGRADUATE,
+                smokingStatus=SmokingStatus.FORMER,
+                alcohol=AlcoholCategory.NONE,
+                antiHypertensiveCount=0,
+                statin=0,
+                otherLipidLoweringMedicationCount=0,
+                initializeAfib=(lambda _: None),
+                randomEffects={'gcp': 0},
+            )
+            base_gcp = GCPModel().calc_linear_predictor(test_person)
+            test_person._gcp.append([base_gcp])
+
+            people = [test_person]
             population = Population(people)
             VectorizedTestFixture._population_dataframe = population.get_people_current_state_and_summary_as_dataframe()
         return VectorizedTestFixture._population_dataframe
