57a7282f09c78afa10af52e74daf5f0d368d8964
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-04-23 16:20:00 -0400

    switch test_cox_model from fixture to helper

--- microsim/test/test_cox_model.py
+++ microsim/test/test_cox_model.py
@@ -1,12 +1,46 @@
+import unittest
+
+from microsim.alcohol_category import AlcoholCategory
 from microsim.cox_regression_model import CoxRegressionModel
 from microsim.data_loader import load_model_spec
+from microsim.education import Education
+from microsim.gender import NHANESGender
+from microsim.person import Person
+from microsim.race_ethnicity import NHANESRaceEthnicity
+from microsim.smoking_status import SmokingStatus
 from microsim.statsmodel_cox_model import StatsModelCoxModel
-from microsim.test.fixture.vectorized_test_fixture import VectorizedTestFixture
+from microsim.test.helper.init_vectorized_population_dataframe import (
+    init_vectorized_population_dataframe
+)
 
 
-class TestCoxModel(VectorizedTestFixture):
+class TestCoxModel(unittest.TestCase):
     def setUp(self):
-        super().setUp()
+        self.population_dataframe = init_vectorized_population_dataframe([
+            Person(
+                age=71,
+                gender=NHANESGender.MALE,
+                raceEthnicity=NHANESRaceEthnicity.NON_HISPANIC_WHITE,
+                sbp=144.667,
+                dbp=52.6667,
+                a1c=9.5,
+                hdl=34,
+                totChol=191,
+                bmi=30.05,
+                ldl=110.0,
+                trig=128,
+                waist=45,
+                anyPhysicalActivity=0,
+                education=Education.COLLEGEGRADUATE,
+                smokingStatus=SmokingStatus.FORMER,
+                alcohol=AlcoholCategory.NONE,
+                antiHypertensiveCount=0,
+                statin=0,
+                otherLipidLoweringMedicationCount=0,
+                initializeAfib=(lambda _: None),
+                randomEffects={'gcp': 0},
+            ),
+        ], with_base_gcp=True)
         model_spec = load_model_spec("nhanesMortalityModel")
         self.model = StatsModelCoxModel(CoxRegressionModel(**model_spec))
 
