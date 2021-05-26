ad5055c9433dba5264c5b3dd43dff85b349582d0
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-05-26 15:44:08 -0400

    add dataclass for dynamic data of BPCOG person

---
+++ microsim/person/bpcog_person_dynamic_data.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from microsim.alcohol_category import AlcoholCategory
+
+
+@dataclass
+class BPCOGPersonDynamicData:
+    """Dataclass containing dynamic data of a single BPCOG Person for one time tick."""
+    person_id: int
+    alive: bool
+    age: int
+    sbp: int
+    dbp: int
+    a1c: float
+    hdl: int
+    ldl: int
+    trig: int
+    tot_chol: int
+    bmi: float
+    waist: int
+    any_physical_activity: bool
+    alcohol_per_week: AlcoholCategory
+    anti_hypertensive_count: int
+    statin: int
+    other_lipid_lower_medication: int
+    bp_meds_added: int
+    afib: bool
+    qalys: float
+    gcp: float
