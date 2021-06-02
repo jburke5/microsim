5438f9d573badeb03396c8f82edd7c17fc9bb997
llc2 <45376498+llc2@users.noreply.github.com>
2021-06-02 15:41:05 -0400

    Switch Code Formatter to Black (#17)
    * add bblack as dev dependency
    
    * call black in poetry format script
    
    also removed format-diff option, which black would not support
    
    * remove autopep8 from dev dependencies
    
    * update docs to reflect black as new formatter
    
    * remove line-length from example VSCode settings
    
    setting is redundant with line length being set in pyproject.toml
    
    * enable formatOnSave in example VSCode settings

--- scripts/format.py
+++ scripts/format.py
@@ -1,16 +1,7 @@
 import subprocess
 
 
-shared_autopep8_options = ["--aggressive", "--aggressive"]
-
-
 def main():
-    cmd = ["autopep8", "--in-place", "--recursive", "."] + shared_autopep8_options
-    proc = subprocess.run(cmd)
-    exit(proc.returncode)
-
-
-def diffmain():
-    cmd = ["autopep8", "--diff", "--recursive", "."] + shared_autopep8_options
+    cmd = ["black", "."]
     proc = subprocess.run(cmd)
     exit(proc.returncode)
