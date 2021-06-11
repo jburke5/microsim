59ec0e94d56008f50183f777c9cb9d314966a71b
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-11 14:20:23 -0400

    initial commit of  NumpyPersonStore
    only static data for now. Interfaces likely to change

---
+++ microsim/numpy_person_store.py
@@ -0,0 +1,16 @@
+from typing import Generic, TypeVar, List
+import numpy as np
+
+
+S = TypeVar("S")  # _S_tatic data type
+
+
+class NumpyPersonStore(Generic[S]):
+    """Holds Person data in numpy ndarrays."""
+
+    def __init__(self, static_data: List[S]):
+        static_type = type(static_data[0])
+        self._static_data_array = np.array(
+            [s.as_numpy_arraylike() for s in static_data],
+            dtype=static_type.get_numpy_dtype(),
+        )
