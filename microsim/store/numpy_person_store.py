caa87e52fd284a7bdf70fdf7bc1c4c18863aeed1
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-21 18:39:54 -0400

    add static data converter to NumpyPersonStore init

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -1,6 +1,6 @@
 from typing import Generic, List, TypeVar
 import numpy as np
-
+from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProtocol
 
 
 S = TypeVar("S")  # _S_tatic data type
@@ -9,9 +9,8 @@ S = TypeVar("S")  # _S_tatic data type
 class NumpyPersonStore(Generic[S]):
     """Holds Person data in numpy ndarrays."""
 
-    def __init__(self, static_data: List[S]):
-        static_type = type(static_data[0])
+    def __init__(self, static_data: List[S], static_data_converter: NumpyDataConverterProtocol[S]):
         self._static_data_array = np.array(
-            [s.as_numpy_arraylike() for s in static_data],
-            dtype=static_type.get_numpy_dtype(),
+            [static_data_converter.to_row_tuple(s) for s in static_data],
+            dtype=static_data_converter.get_dtype(),
         )
