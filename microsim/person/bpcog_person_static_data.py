7e4be05cada5638b4c398e19fe35f3010157d203
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-22 17:55:27 -0400

    remove conversion logic from static data class

--- microsim/person/bpcog_person_static_data.py
+++ microsim/person/bpcog_person_static_data.py
@@ -1,15 +1,12 @@
-from dataclasses import dataclass, fields
-import numpy as np
+from dataclasses import dataclass
 from microsim.gender import NHANESGender
 from microsim.race_ethnicity import NHANESRaceEthnicity
 from microsim.education import Education
 from microsim.smoking_status import SmokingStatus
-from microsim.person.pytype_to_nptype import pytype_to_nptype
-from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProtocol
 
 
 @dataclass
-class BPCOGPersonStaticData(NumpyDataConverterProtocol["BPCOGPersonStaticData"]):
+class BPCOGPersonStaticData:
     """Dataclass containing static data of a single Person."""
 
     gender: NHANESGender
@@ -17,12 +14,3 @@ class BPCOGPersonStaticData(NumpyDataConverterProtocol["BPCOGPersonStaticData"])
     education: Education
     smokingStatus: SmokingStatus
     randomEffectsGcp: float
-
-    def get_dtype(self):
-        field_specs = [(f.name, pytype_to_nptype(f.type)) for f in fields(self)]
-        return np.dtype(field_specs)
-
-    def to_row_tuple(self, obj: "BPCOGPersonStaticData"):
-        dtype = obj.get_dtype()
-        values = tuple(getattr(self, name) for name in dtype.names)
-        return values
