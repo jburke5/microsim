678659c5f261c5bf2b41dafc2622496eacab6d06
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-22 18:07:10 -0400

    make field py to np type func configurable

--- microsim/store/dataclass_numpy_data_converter.py
+++ microsim/store/dataclass_numpy_data_converter.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, fields
-from typing import Type
+from typing import Callable, Optional, Type
 import numpy as np
 from microsim.person.pytype_to_nptype import pytype_to_nptype
 from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProtocol
@@ -8,11 +8,14 @@ from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProto
 class DataclassNumpyDataConverter(NumpyDataConverterProtocol):
     """Converts dataclasses into Numpy array rows."""
 
-    def __init__(self, type: Type[dataclass]):
+    def __init__(self, type: Type[dataclass], *, field_pytype_to_nptype: Optional[Callable]):
         self._type = type
+        self._field_pytype_to_nptype = (
+            field_pytype_to_nptype if field_pytype_to_nptype is not None else pytype_to_nptype
+        )
 
     def get_dtype(self):
-        field_specs = [(f.name, pytype_to_nptype(f.type)) for f in fields(self._type)]
+        field_specs = [(f.name, self._field_pytype_to_nptype(f.type)) for f in fields(self._type)]
         return np.dtype(field_specs)
 
     def to_row_tuple(self, obj):
