ed1ed84247b67b88128aedbd7a5e7b6273c836f2
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-22 17:35:48 -0400

    make static person dataclass its own np converter

--- microsim/person/bpcog_person_static_data.py
+++ microsim/person/bpcog_person_static_data.py
@@ -5,10 +5,11 @@ from microsim.race_ethnicity import NHANESRaceEthnicity
 from microsim.education import Education
 from microsim.smoking_status import SmokingStatus
 from microsim.person.pytype_to_nptype import pytype_to_nptype
+from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProtocol
 
 
 @dataclass
-class BPCOGPersonStaticData:
+class BPCOGPersonStaticData(NumpyDataConverterProtocol["BPCOGPersonStaticData"]):
     """Dataclass containing static data of a single Person."""
 
     gender: NHANESGender
@@ -17,16 +18,11 @@ class BPCOGPersonStaticData:
     smokingStatus: SmokingStatus
     randomEffectsGcp: float
 
-    @classmethod
-    def get_numpy_dtype(cls):
-        """Returns np.dtype that numpy can use to represent this class' data."""
-        field_specs = [(f.name, pytype_to_nptype(f.type)) for f in fields(cls)]
+    def get_dtype(self):
+        field_specs = [(f.name, pytype_to_nptype(f.type)) for f in fields(self)]
         return np.dtype(field_specs)
 
-    def as_numpy_arraylike(self):
-        """
-        Returns a tuple that, along with `get_numpy_dtype()`, numpy can use to create an array.
-        """
-        dtype = self.get_numpy_dtype()
+    def to_row_tuple(self, obj: "BPCOGPersonStaticData"):
+        dtype = obj.get_dtype()
         values = tuple(getattr(self, name) for name in dtype.names)
         return values
