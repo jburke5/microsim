f212e66f56f964bc8e69a03278f3915037f8477f
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-23 17:28:20 -0400

    switch Person store from converter proto to abc

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -1,6 +1,6 @@
 from typing import Generic, List, TypeVar
 import numpy as np
-from microsim.store.numpy_data_converter_protocol import NumpyDataConverterProtocol
+from microsim.store.base_numpy_data_converter import BaseNumpyDataConverter
 
 
 S = TypeVar("S")  # _S_tatic data type
@@ -9,7 +9,7 @@ S = TypeVar("S")  # _S_tatic data type
 class NumpyPersonStore(Generic[S]):
     """Holds Person data in numpy ndarrays."""
 
-    def __init__(self, static_data: List[S], static_data_converter: NumpyDataConverterProtocol[S]):
+    def __init__(self, static_data: List[S], static_data_converter: BaseNumpyDataConverter[S]):
         self._static_data_array = np.array(
             [static_data_converter.to_row_tuple(s) for s in static_data],
             dtype=static_data_converter.get_dtype(),
