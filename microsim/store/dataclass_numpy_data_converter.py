3335c1479699cfe56b945a4e475e382da39aa9fd
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-22 20:30:34 -0400

    add manual type checks for dc data converter

--- microsim/store/dataclass_numpy_data_converter.py
+++ microsim/store/dataclass_numpy_data_converter.py
@@ -1,4 +1,4 @@
-from dataclasses import fields
+from dataclasses import fields, is_dataclass
 from typing import Any, Callable, Optional, Type
 import numpy as np
 from microsim.person.pytype_to_nptype import pytype_to_nptype
@@ -9,6 +9,8 @@ class DataclassNumpyDataConverter(NumpyDataConverterProtocol):
     """Converts dataclasses into Numpy array rows."""
 
     def __init__(self, type: Type, *, field_pytype_to_nptype: Optional[Callable[[Type], Any]]):
+        if not is_dataclass(type):
+            raise TypeError(f"Given argument `type` is not a dataclass: {type}")
         self._type = type
         self._field_pytype_to_nptype = (
             field_pytype_to_nptype if field_pytype_to_nptype is not None else pytype_to_nptype
@@ -19,6 +21,11 @@ class DataclassNumpyDataConverter(NumpyDataConverterProtocol):
         return np.dtype(field_specs)
 
     def to_row_tuple(self, obj):
+        if not isinstance(obj, self._type):
+            raise TypeError(
+                f"Given argument `obj` is not an instance of the configured type ({self._type}):"
+                f" {obj}"
+            )
         dtype = self.get_dtype()
         values = tuple(getattr(obj, name) for name in dtype.names)
         return values
