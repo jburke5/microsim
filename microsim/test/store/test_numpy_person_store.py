68442d52ce0a5dd483315b76fccd0bc76866d5cc
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-24 17:12:29 -0400

    test mismatched array lengths with store init

--- microsim/test/store/test_numpy_person_store.py
+++ microsim/test/store/test_numpy_person_store.py
@@ -15,16 +15,45 @@ class DynamicTestData:
 
 
 class TestNumpyPersonStore(TestCase):
+    def setUp(self):
+        self._static_data = [StaticTestData(20), StaticTestData(40), StaticTestData(80)]
+        self._static_data_converter = DataclassNumpyDataConverter(StaticTestData)
+
+        self._dynamic_data = [DynamicTestData(s.starting_age) for s in self._static_data]
+        self._dynamic_data_converter = DataclassNumpyDataConverter(DynamicTestData)
+
     def test_get_num_persons_canonical_returns_num_persons(self):
-        static_data = [StaticTestData(20), StaticTestData(40), StaticTestData(80)]
-        static_data_converter = DataclassNumpyDataConverter(StaticTestData)
-        dynamic_data = [DynamicTestData(s.starting_age) for s in static_data]
-        dynamic_data_converter = DataclassNumpyDataConverter(DynamicTestData)
         store = NumpyPersonStore(
-            static_data, static_data_converter, dynamic_data, dynamic_data_converter
+            self._static_data,
+            self._static_data_converter,
+            self._dynamic_data,
+            self._dynamic_data_converter,
         )
         expected_num_persons = 3
 
         actual_num_persons = store.get_num_persons()
-
         self.assertEqual(expected_num_persons, actual_num_persons)
+
+    def test_init_static_data_shorter_raises_error(self):
+        static_data = self._static_data[:2]
+        expected_msg = "Lengths of `static_data` and `dynamic_data` args do not match: 2 != 3"
+
+        with self.assertRaises(ValueError, msg=expected_msg):
+            NumpyPersonStore(
+                static_data,
+                self._static_data_converter,
+                self._dynamic_data,
+                self._dynamic_data_converter,
+            )
+
+    def test_init_dynamic_data_shorter_raises_error(self):
+        dynamic_data = self._dynamic_data[:1]
+        expected_msg = "Lengths of `static_data` and `dynamic_data` args do not match: 3 != 1"
+
+        with self.assertRaises(ValueError, msg=expected_msg):
+            NumpyPersonStore(
+                self._static_data,
+                self._static_data_converter,
+                dynamic_data,
+                self._dynamic_data_converter,
+            )
