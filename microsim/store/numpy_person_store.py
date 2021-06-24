fe6eb60533d0b37c6cd802ad789071272a6fbb76
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-24 17:23:08 -0400

    add event data to numpy person store

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -5,9 +5,10 @@ from microsim.store.base_numpy_data_converter import BaseNumpyDataConverter
 
 S = TypeVar("S")  # _S_tatic data type
 D = TypeVar("D")  # _D_ynamic data type
+E = TypeVar("E")  # _E_vent data type
 
 
-class NumpyPersonStore(Generic[S, D]):
+class NumpyPersonStore(Generic[S, D, E]):
     """Holds Person data in numpy ndarrays."""
 
     def __init__(
@@ -16,15 +17,18 @@ class NumpyPersonStore(Generic[S, D]):
         static_data_converter: BaseNumpyDataConverter[S],
         dynamic_data: List[D],
         dynamic_data_converter: BaseNumpyDataConverter[D],
+        event_data: List[E],
+        event_data_converter: BaseNumpyDataConverter[E],
     ):
         len_static = len(static_data)
         len_dynamic = len(dynamic_data)
-        if len_static != len_dynamic:
+        len_event = len(event_data)
+        if not (len_static == len_dynamic == len_event):
             raise ValueError(
-                "Lengths of `static_data` and `dynamic_data` args do not match:"
-                f" {len_static} != {len_dynamic}"
+                "Lengths of `static_data`, `dynamic_data`, and `event_data` args do not match:"
+                f" {len_static}, {len_dynamic}, {len_event}"
             )
-        self._num_persons = len_static
+        self._num_persons = len_static  # lengths asserts to be the same: chose static arbitrarily
 
         static_dtype = static_data_converter.get_dtype()
         static_data_arraylike = [static_data_converter.to_row_tuple(s) for s in static_data]
@@ -33,6 +37,10 @@ class NumpyPersonStore(Generic[S, D]):
         dynamic_dtype = dynamic_data_converter.get_dtype()
         dynamic_data_arraylike = [dynamic_data_converter.to_row_tuple(d) for d in dynamic_data]
         self._dynamic_data_array = np.array(dynamic_data_arraylike, dtype=dynamic_dtype, ndmin=3)
+
+        event_dtype = event_data_converter.get_dtype()
+        event_data_arraylike = [event_data_converter.to_row_tuple(e) for e in event_data]
+        self._event_data_array = np.array(event_data_arraylike, dtype=event_dtype, ndmin=3)
 
     def get_num_persons(self):
         """Returns the number of people held in this store."""
