a927eb2841b8986f259b199581d9f43f0db570c7
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-06-29 16:30:58 -0400

    change inherit_annotations to use all bases

--- microsim/person/inherit_annotations.py
+++ microsim/person/inherit_annotations.py
@@ -1,13 +1,13 @@
 def inherit_annotations(cls):
     """
-    Inherits class attribute annotations from immediate base classes.
+    Inherits class attribute annotations from all base classes.
 
     Convenience function for creating dataclasses from Protocols that define
     the class attributes but do not implement them.
     """
 
     annotations = {}
-    for base_class in reversed(cls.__bases__):
+    for base_class in reversed(cls.__mro__):
         annotations.update(base_class.__annotations__)
     cls.__annotations__ = annotations
     return cls
