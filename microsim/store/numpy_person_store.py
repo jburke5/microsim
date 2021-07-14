e14a4cf8cc26d0422ba1d85dbab83155d1864d60
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-14 11:36:50 -0400

    change store index order to tick, person index
    simulation will think of Population primarily at points in time vs.
    individual Persons, though reporting/analytics will likely think more in
    terms of individuals. Can revisit this if these assumptions are false or
    if needs change though

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -32,15 +32,15 @@ class NumpyPersonStore:
 
         dynamic_dtype = dynamic_data_converter.get_dtype()
         dynamic_data_arraylike = [dynamic_data_converter.to_row_tuple(d) for d in dynamic_data]
-        dynamic_shape = (self._num_persons, self._num_years)
+        dynamic_shape = (self._num_years, self._num_persons)
         self._dynamic_data_array = np.rec.array(np.zeros(dynamic_shape, dtype=dynamic_dtype))
-        self._dynamic_data_array[:, 0] = dynamic_data_arraylike
+        self._dynamic_data_array[0] = dynamic_data_arraylike
 
         event_dtype = event_data_converter.get_dtype()
         event_data_arraylike = [event_data_converter.to_row_tuple(e) for e in event_data]
-        event_shape = (self._num_persons, self._num_years)
+        event_shape = (self._num_years, self._num_persons)
         self._event_data_array = np.rec.array(np.zeros(event_shape, dtype=event_dtype))
-        self._event_data_array[:, 0] = event_data_arraylike
+        self._event_data_array[0] = event_data_arraylike
 
     def get_num_persons(self):
         """Returns the number of people held in this store."""
