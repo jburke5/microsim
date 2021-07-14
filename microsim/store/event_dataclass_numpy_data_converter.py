a7feaff542c0923495cd293810538f66799aff97
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-14 11:21:40 -0400

    use empty string to indicate no Outcome type
    Numpy converts None to 'None' when casting to np.unicode_, so using
    empty string to mean "no type" to avoid ambiguity, e.g., with a
    hypothetical OutcomeType of 'None". Also taking as further motivation to
    remove strings from Outcome/Events entirely.

--- microsim/store/event_dataclass_numpy_data_converter.py
+++ microsim/store/event_dataclass_numpy_data_converter.py
@@ -26,16 +26,17 @@ class EventDataclassNumpyDataConverter(BaseNumpyDataConverter):
                 f"Given argument `obj` is not an instance of `BPCOGPersonEventRecord`: {obj}"
             )
 
+        empty_string = ""  # Numpy converts None to 'None' for unicode dtypes, hence empty string
         has_mi = obj.mi is not None
-        mi_type = obj.mi.type.value if has_mi else None
+        mi_type = obj.mi.type.value if has_mi else empty_string
         mi_fatal = obj.mi.fatal if has_mi else False
 
         has_stroke = obj.stroke is not None
-        stroke_type = obj.stroke.type.value if has_stroke else None
+        stroke_type = obj.stroke.type.value if has_stroke else empty_string
         stroke_fatal = obj.stroke.fatal if has_stroke else False
 
         has_dementia = obj.dementia is not None
-        dementia_type = obj.dementia.type.value if has_dementia else None
+        dementia_type = obj.dementia.type.value if has_dementia else empty_string
         dementia_fatal = obj.dementia.fatal if has_dementia else False
 
         values = (
