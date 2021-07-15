5873bf7159e988aaa43e3a7e508288fcb5f831b8
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-15 15:12:02 -0400

    save converters in numpy person store init

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -26,18 +26,23 @@ class NumpyPersonStore:
 
         self._num_years = int(num_years)
 
-        static_dtype = static_data_converter.get_dtype()
-        static_data_arraylike = [static_data_converter.to_row_tuple(s) for s in static_data]
+        self._static_data_converter = static_data_converter
+        static_dtype = self._static_data_converter.get_dtype()
+        static_data_arraylike = [self._static_data_converter.to_row_tuple(s) for s in static_data]
         self._static_data_array = np.rec.array(static_data_arraylike, dtype=static_dtype)
 
-        dynamic_dtype = dynamic_data_converter.get_dtype()
-        dynamic_data_arraylike = [dynamic_data_converter.to_row_tuple(d) for d in dynamic_data]
+        self._dynamic_data_converter = dynamic_data_converter
+        dynamic_dtype = self._dynamic_data_converter.get_dtype()
+        dynamic_data_arraylike = [
+            self._dynamic_data_converter.to_row_tuple(d) for d in dynamic_data
+        ]
         dynamic_shape = (self._num_years, self._num_persons)
         self._dynamic_data_array = np.rec.array(np.zeros(dynamic_shape, dtype=dynamic_dtype))
         self._dynamic_data_array[0] = dynamic_data_arraylike
 
-        event_dtype = event_data_converter.get_dtype()
-        event_data_arraylike = [event_data_converter.to_row_tuple(e) for e in event_data]
+        self._event_data_converter = event_data_converter
+        event_dtype = self._event_data_converter.get_dtype()
+        event_data_arraylike = [self._event_data_converter.to_row_tuple(e) for e in event_data]
         event_shape = (self._num_years, self._num_persons)
         self._event_data_array = np.rec.array(np.zeros(event_shape, dtype=event_dtype))
         self._event_data_array[0] = event_data_arraylike
