d8f5d8f5ca65299eef1e2923dc62f3b3b0f6694b
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-20 17:14:43 -0400

    save numpy converters to population proxy
    needed to construct person proxies on the fly

--- microsim/store/numpy_population_record_proxy.py
+++ microsim/store/numpy_population_record_proxy.py
@@ -1,5 +1,16 @@
 class NumpyPopulationRecordProxy:
-    def __init__(self, static_rows, dynamic_rows, event_rows):
+    def __init__(
+        self,
+        static_rows,
+        dynamic_rows,
+        event_rows,
+        static_converter,
+        dynamic_converter,
+        event_converter,
+    ):
         self._static_rows = static_rows
         self._dynamic_rows = dynamic_rows
         self._event_rows = event_rows
+        self._static_converter = static_converter
+        self._dynamic_converter = dynamic_converter
+        self._event_converter = event_converter
