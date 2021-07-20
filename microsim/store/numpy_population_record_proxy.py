d6157378886550eaacada48327c3a1c8c9beeb2b
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-20 17:15:49 -0400

    add apply method to population proxy

--- microsim/store/numpy_population_record_proxy.py
+++ microsim/store/numpy_population_record_proxy.py
@@ -1,3 +1,7 @@
+from microsim.store.numpy_person_record_proxy import NumpyPersonRecordProxy
+import numpy as np
+
+
 class NumpyPopulationRecordProxy:
     def __init__(
         self,
@@ -14,3 +18,26 @@ class NumpyPopulationRecordProxy:
         self._static_converter = static_converter
         self._dynamic_converter = dynamic_converter
         self._event_converter = event_converter
+
+    def apply(self, func, **kwargs):
+        """
+        Applies `func` to each person record, then returns the result.
+
+        Any keyword arguments that are not used will be passed to `func`.
+        """
+        ops = [self._static_rows, self._dynamic_rows, self._event_rows, None]
+        flags = []
+        op_flags = [["readonly"], ["readonly"], ["readonly"], ["writeonly"]]
+        op_dtypes = [
+            self._static_converter.get_dtype(),
+            self._dynamic_converter.get_dtype(),
+            self._event_converter.get_dtype(),
+            np.float64,
+        ]
+        with np.nditer(ops, flags, op_flags, op_dtypes) as it:
+            for s, d, e, out in it:
+                record_proxy = NumpyPersonRecordProxy(
+                    s, d, e, self._static_converter, self._dynamic_converter, self._event_converter
+                )
+                out[...] = func(record_proxy, **kwargs)
+            return out
