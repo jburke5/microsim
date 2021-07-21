35fa03597ff95c4f1159fd7c2eb48dcf1853cd8d
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-21 13:55:53 -0400

    add filter_inplace, mask to population proxy
    allows skipping person records who do not meet a condition (e.g., if
    the person is no longer alive.)

--- microsim/store/numpy_population_record_proxy.py
+++ microsim/store/numpy_population_record_proxy.py
@@ -1,3 +1,4 @@
+from itertools import repeat
 from microsim.store.numpy_person_record_proxy import NumpyPersonRecordProxy
 import numpy as np
 
@@ -18,24 +19,32 @@ class NumpyPopulationRecordProxy:
         self._static_converter = static_converter
         self._dynamic_converter = dynamic_converter
         self._event_converter = event_converter
+        self._mask = None
 
-    def apply(self, func, **kwargs):
+    def filter_inplace(self, condition):
+        """Hides rows that do not meet the given condition."""
+        self._mask = self.apply(condition, out_dtype=np.bool8)
+
+    def apply(self, func, out_dtype=np.float64, **kwargs):
         """
         Applies `func` to each person record, then returns the result.
 
         Any keyword arguments that are not used will be passed to `func`.
         """
-        ops = [self._static_rows, self._dynamic_rows, self._event_rows, None]
+        mask = self._mask if self._mask is not None else repeat(True)
+        ops = [mask, self._static_rows, self._dynamic_rows, self._event_rows, None]
         flags = []
-        op_flags = [["readonly"], ["readonly"], ["readonly"], ["writeonly"]]
+        op_flags = [["readonly"], ["readonly"], ["readonly"], ["readonly"], ["writeonly"]]
         op_dtypes = [
             self._static_converter.get_dtype(),
             self._dynamic_converter.get_dtype(),
             self._event_converter.get_dtype(),
-            np.float64,
+            out_dtype,
         ]
         with np.nditer(ops, flags, op_flags, op_dtypes) as it:
-            for s, d, e, out in it:
+            for m, s, d, e, out in it:
+                if not m:
+                    continue
                 record_proxy = NumpyPersonRecordProxy(
                     s, d, e, self._static_converter, self._dynamic_converter, self._event_converter
                 )
