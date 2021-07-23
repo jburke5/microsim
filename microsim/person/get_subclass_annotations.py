899d28d63f1f5d1d7c55bdaf46777d2896648344
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-23 15:45:58 -0400

    add get subclass annotations helper

---
+++ microsim/person/get_subclass_annotations.py
@@ -0,0 +1,10 @@
+def get_subclass_annotations(cls):
+    """Returns dict of class attr annotations from all base classes."""
+
+    annotations = {}
+    for base_class in reversed(cls.__mro__):
+        try:
+            annotations.update(base_class.__annotations__)
+        except AttributeError:
+            pass  # okay if not all classes have __annotations__
+    return annotations
--- microsim/person/inherit_annotations.py
+++ microsim/person/inherit_annotations.py
@@ -1,3 +1,6 @@
+from microsim.person.get_subclass_annotations import get_subclass_annotations
+
+
 def inherit_annotations(cls):
     """
     Inherits class attribute annotations from all base classes.
@@ -6,11 +9,5 @@ def inherit_annotations(cls):
     the class attributes but do not implement them.
     """
 
-    annotations = {}
-    for base_class in reversed(cls.__mro__):
-        try:
-            annotations.update(base_class.__annotations__)
-        except AttributeError:
-            pass  # okay if not all classes have __annotations__
-    cls.__annotations__ = annotations
+    cls.__annotations__ = get_subclass_annotations(cls)
     return cls
