7091aed1b887ad8db943bfcf55668541467fd936
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-26 14:51:37 -0400

    use get_type_hints in get_base_annotations

--- microsim/util/get_base_annotations.py
+++ microsim/util/get_base_annotations.py
@@ -1,10 +1,14 @@
+from typing import get_type_hints
+
+
 def get_base_annotations(cls):
     """Returns dict of class attr annotations from all base classes."""
 
     annotations = {}
     for base_class in reversed(cls.__mro__):
         try:
-            annotations.update(base_class.__annotations__)
+            base_class_annotations = get_type_hints(base_class)
+            annotations.update(base_class_annotations)
         except AttributeError:
             pass  # okay if not all classes have __annotations__
     return annotations
