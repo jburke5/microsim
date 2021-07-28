8f35fa300331d47e25f6e3fb6694ab0e81d21055
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-28 17:58:33 -0400

    add hard-coded event record mappping

--- microsim/store/numpy_subrecord_mapping.py
+++ microsim/store/numpy_subrecord_mapping.py
@@ -1,7 +1,8 @@
 from enum import IntEnum
 import numpy as np
-from microsim.util.get_base_annotations import get_base_annotations
 from microsim.store.numpy_scalar_mapping import NumpyScalarMapping
+from microsim.store.numpy_struct_mapping import NumpyStructMapping
+from microsim.util.get_base_annotations import get_base_annotations
 
 
 def get_enum_member_value(x):
@@ -10,6 +11,14 @@ def get_enum_member_value(x):
 
 def identity(x):
     return x
+
+
+def new_outcome_struct_mapping(field_name):
+    scalar_mappings = [
+        NumpyScalarMapping("type", "U9", identity, identity),
+        NumpyScalarMapping("fatal", np.bool8, identity, bool),
+    ]
+    return NumpyStructMapping(field_name, scalar_mappings)
 
 
 def get_scalar_mapping(field_name, pytype):
@@ -23,6 +32,26 @@ def get_scalar_mapping(field_name, pytype):
         return NumpyScalarMapping(field_name, np.float64, identity, float)
 
     raise NotImplementedError(f"Scalar mapping not implemented for Python type: {pytype}")
+
+
+class NumpyEventSubrecordMapping:
+    def __init__(self):
+        self._property_mappings = {
+            p: new_outcome_struct_mapping(p) for p in ["mi", "stroke", "dementia"]
+        }
+        dtype_fields = [
+            (p, [(s.field_name, s.field_name) for s in m.scalar_fields])
+            for p, m in self._property_mappings.items()
+        ]
+        self._dtype = np.dtype(dtype_fields)
+
+    @property
+    def property_mappings(self):
+        return self._property_mappings
+
+    @property
+    def dtype(self):
+        return self._dtype
 
 
 class NumpySubrecordMapping:
