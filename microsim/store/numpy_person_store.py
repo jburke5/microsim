c37cb84d87019c039d66f871debd43b5aa48e249
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-07-29 13:10:34 -0400

    add new_person_record_proxy helper

--- microsim/store/numpy_person_store.py
+++ microsim/store/numpy_person_store.py
@@ -1,3 +1,4 @@
+from microsim.store.numpy_field_proxy import NumpyFieldProxy
 import numpy as np
 from microsim.store.numpy_person_record_proxy import NumpyPersonRecordProxy
 from microsim.store.numpy_population_record_proxy import NumpyPopulationRecordProxy
@@ -10,6 +11,41 @@ def assert_positive_int(value, name):
         )
     if value <= 0:
         raise ValueError(f"Expected `{name}` to be a positive integer; received: {value}")
+
+
+def assert_unique_prop_names(static_prop_names, dynamic_prop_names, event_prop_names):
+    static_dynamic_overlap = static_prop_names & dynamic_prop_names
+    static_event_overlap = static_prop_names & event_prop_names
+    dynamic_event_overlap = dynamic_prop_names & event_prop_names
+
+    if static_dynamic_overlap | static_event_overlap | dynamic_event_overlap:
+        overlap_list = [
+            (("static", "dynamic"), static_dynamic_overlap),
+            (("static", "event"), static_event_overlap),
+            (("dynamic", "event"), dynamic_event_overlap),
+        ]
+        overlap_dict = {k: v for k, v in overlap_list if v}
+        raise ValueError(f"Duplicate property names across subrecords: {overlap_dict}")
+
+
+def proxy_attrs_from_props(property_mappings, row_attr_name):
+    return {
+        prop_name: NumpyFieldProxy(
+            row_attr_name, mapping.field_name, mapping.to_np, mapping.from_np
+        )
+        for prop_name, mapping in property_mappings.items()
+    }
+
+
+def new_person_record_proxy_class(static_props, dynamic_props, event_props):
+    assert_unique_prop_names(static_props.keys(), dynamic_props.keys(), event_props.keys())
+
+    static_attrs = proxy_attrs_from_props(static_props, "_static_row")
+    dynamic_attrs = proxy_attrs_from_props(dynamic_props, "_dynamic_row")
+    event_attrs = proxy_attrs_from_props(event_props, "_event_row")
+    proxy_class_attrs = {**static_attrs, **dynamic_attrs, **event_attrs}
+    person_record_proxy_class = type("NumpyPersonRecordProxy", tuple(), proxy_class_attrs)
+    return person_record_proxy_class
 
 
 class NumpyPersonStore:
@@ -40,6 +76,12 @@ class NumpyPersonStore:
         event_shape = (self._num_ticks + 1, self._num_persons)  # + 1 for initial data + k ticks
         event_dtype = event_mapping.dtype
         self._event_data_array = np.zeros(event_shape, event_dtype)
+
+        self._person_record_proxy_class = new_person_record_proxy_class(
+            static_mapping.property_mappings,
+            dynamic_mapping.property_mappings,
+            event_mapping.property_mappings,
+        )
 
     def get_num_persons(self):
         """Returns the number of people held in this store."""
