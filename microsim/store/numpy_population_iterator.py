44e4a3a0c88f48934bcc8286e48730e9cb4e56e2
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-08-09 16:03:30 -0400

    use store.get_person_record in population iterator

--- microsim/store/numpy_population_iterator.py
+++ microsim/store/numpy_population_iterator.py
@@ -3,24 +3,9 @@ import numpy as np
 
 
 class NumpyPopulationIterator:
-    def __init__(
-        self, static_data, dynamic_data, event_data, active_indices, new_person_record_proxy
-    ):
-        self._static_data = static_data
-        self._dynamic_data = dynamic_data
-        self._event_data = event_data
-        self._new_person_record_proxy = new_person_record_proxy
-
-        assert self._static_data.ndim == 1
-        assert self._dynamic_data.ndim == 2
-        assert self._event_data.ndim == 2
-
-        assert self._dynamic_data.shape[0] == self._event_data.shape[0]
-        assert (
-            self._static_data.shape[0] == self._dynamic_data.shape[1] == self._event_data.shape[1]
-        )
-
-        self._num_ticks = self._dynamic_data.shape[0]
+    def __init__(self, person_store, at_t, active_indices):
+        self._person_store = person_store
+        self._at_t = at_t
         self._it = np.nditer(active_indices, [], ["readonly"], [active_indices.dtype])
 
     def __iter__(self):
@@ -28,17 +13,10 @@ class NumpyPopulationIterator:
 
     def __next__(self):
         abs_person_idx = next(self._it)
-
-        static_record = self._static_data[abs_person_idx]
-        all_record_proxies = [
-            self._new_person_record_proxy(
-                static_record,
-                self._dynamic_data[t][abs_person_idx],
-                self._event_data[t][abs_person_idx],
-            )
-            for t in range(self._num_ticks)
+        all_person_records = [
+            self._person_store.get_person_record(abs_person_idx, t) for t in range(self._at_t + 1)
         ]
-        cur_proxy = all_record_proxies[-1]
-        all_prev_proxies = all_record_proxies[:-1]
+        cur_proxy = all_person_records[-1]
+        all_prev_proxies = all_person_records[:-1]
         person_proxy = NumpyPersonProxy(cur_proxy, all_prev_proxies)
         return person_proxy
