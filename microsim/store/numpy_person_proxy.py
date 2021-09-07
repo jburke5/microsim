24ad7f43fea61e162d4df4e35a10598cf43b2760
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-09-07 17:05:15 -0400

    proxy latest, mean dynamic props on PersonProxy
    should make PersonProxy compatible with vectorized models. While hacky,
    it would allow to focus on the Person and Population parts of the
    Person-Population refactor and revisit models later

--- microsim/store/numpy_person_proxy.py
+++ microsim/store/numpy_person_proxy.py
@@ -1,7 +1,27 @@
+from itertools import chain
+
+
 class NumpyPersonProxy:
     def __init__(self, next_record, cur_prev_records):
         self._next_record = next_record
         self._cur_prev_records = list(cur_prev_records)
+
+        # proxy latest properties for vectorized model compatibility
+        cur_record = self._cur_prev_records[-1]
+        field_metadata = cur_record.__field_metadata__
+        all_prop_names = set(chain(*[c.keys() for c in field_metadata.values()]))
+
+        for prop_name in all_prop_names:
+            setattr(self, prop_name, property(lambda _: getattr(cur_record, prop_name)))
+
+        # add mean{prop_name} property for numeric dynamic fields
+        num_cur_prev_records = len(self._cur_prev_records)
+        for prop_name, field_mapping in field_metadata["dynamic"].items():
+            mean_prop_name = f"mean{prop_name.capitalize()}"
+            mean_val = (
+                sum([getattr(r, prop_name) for r in self._cur_prev_records]) / num_cur_prev_records
+            )
+            setattr(self, mean_prop_name, property(lambda _: mean_val))
 
     @property
     def next(self):
