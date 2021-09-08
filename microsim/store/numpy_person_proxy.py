2c8cd6ea48778511eef309cc54a38de7761cba64
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-09-08 15:29:13 -0400

    define descriptors for PersonProxy props

--- microsim/store/numpy_person_proxy.py
+++ microsim/store/numpy_person_proxy.py
@@ -1,6 +1,27 @@
 from itertools import chain
 
 
+class DirectProxiedProperty:
+    def __init__(self, proxied_prop_name):
+        self._proxied_prop_name = proxied_prop_name
+
+    def __get__(self, instance, owner=None):
+        cur_record = instance.current
+        value = getattr(cur_record, self._proxied_prop_name)
+        return value
+
+
+class MeanProxiedProperty:
+    def __init__(self, proxied_prop_name):
+        self._proxied_prop_name = proxied_prop_name
+
+    def __get__(self, instance, owner=None):
+        cur_prev_records = instance.current_and_previous
+        values = [getattr(r, self._proxied_prop_name) for r in cur_prev_records]
+        mean = sum(values) / len(cur_prev_records)
+        return mean
+
+
 def new_person_proxy_class(person_record_proxy_class):
     def person_proxy_init(self, next_record, cur_prev_records):
         self._next_record = next_record
@@ -19,17 +40,12 @@ def new_person_proxy_class(person_record_proxy_class):
 
     prop_attrs = {}
     for prop_name in all_record_prop_names:
-        prop_attrs[prop_name] = property(lambda self: getattr(self.current, prop_name))
+        prop_attrs[prop_name] = DirectProxiedProperty(prop_name)
 
     # add mean{prop_name} property for numeric dynamic fields
     for dynamic_prop_name in field_metadata["dynamic"].keys():
         mean_prop_name = f"mean{dynamic_prop_name.capitalize()}"
-        prop_attrs[mean_prop_name] = property(
-            lambda self: (
-                sum([getattr(r, dynamic_prop_name) for r in self.current_and_previous])
-                / len(self.current_and_previous)
-            )
-        )
+        prop_attrs[mean_prop_name] = MeanProxiedProperty(dynamic_prop_name)
 
     proxy_class_attrs = {**prop_attrs, **base_attrs}
     person_proxy_class = type("NumpyPersonProxy", tuple(), proxy_class_attrs)
