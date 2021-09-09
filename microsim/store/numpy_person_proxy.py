594b7aa8759f2466ec9869ae7b58dbb23bcadbd8
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-09-09 13:11:40 -0400

    reintroduce NumpyPersonProxy

--- microsim/store/numpy_person_proxy.py
+++ microsim/store/numpy_person_proxy.py
@@ -50,3 +50,21 @@ def new_person_proxy_class(person_record_proxy_class):
     proxy_class_attrs = {**prop_attrs, **base_attrs}
     person_proxy_class = type("NumpyPersonProxy", tuple(), proxy_class_attrs)
     return person_proxy_class
+
+
+class NumpyPersonProxy:
+    def __init__(self, next_record, cur_prev_records):
+        self._next_record = next_record
+        self._cur_prev_records = list(cur_prev_records)
+
+    @property
+    def next(self):
+        return self._next_record
+
+    @property
+    def current(self):
+        return self._cur_prev_records[-1]
+
+    @property
+    def current_and_previous(self):
+        return self._cur_prev_records
