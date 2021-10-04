4b776732cd26d79e97cea197ea98382e2e6ea4d8
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-10-04 17:34:47 -0400

    add Population.where

--- microsim/store/numpy_population_proxy.py
+++ microsim/store/numpy_population_proxy.py
@@ -1,4 +1,9 @@
+from typing import Callable
+
+import numpy as np
+from microsim.store.numpy_person_proxy import NumpyPersonProxy
 from microsim.store.numpy_population_iterator import NumpyPopulationIterator
+from microsim.store.numpy_subpopulation_proxy import NumpySubpopulationProxy
 
 
 class NumpyPopulationProxy:
@@ -12,3 +17,12 @@ class NumpyPopulationProxy:
     @property
     def num_persons(self):
         return self._person_store.get_num_persons()
+
+    def where(self, condition: Callable[[NumpyPersonProxy], bool]):
+        """Returns subpopulation of persons that satisfy the given condition."""
+        subpop_indices = np.array(
+            [i for i, person in enumerate(self) if condition(person)],
+            dtype=np.intp,
+        )
+        subpop = NumpySubpopulationProxy(self._person_store, self._at_t, subpop_indices)
+        return subpop
