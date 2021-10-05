07c605eb07ab2d59a337d6fb355e8259b2baee45
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-10-05 15:45:08 -0400

    simplify .where indices

--- microsim/store/numpy_population_proxy.py
+++ microsim/store/numpy_population_proxy.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from typing import Callable, Hashable
-import numpy as np
 from microsim.store.numpy_person_proxy import NumpyPersonProxy
 from microsim.store.numpy_population_iterator import NumpyPopulationIterator
 from microsim.store.numpy_subpopulation_proxy import NumpySubpopulationProxy
@@ -20,10 +19,7 @@ class NumpyPopulationProxy:
 
     def where(self, condition: Callable[[NumpyPersonProxy], bool]):
         """Returns subpopulation of persons that satisfy the given condition."""
-        subpop_indices = np.array(
-            [i for i, person in enumerate(self) if condition(person)],
-            dtype=np.intp,
-        )
+        subpop_indices = [i for i, person in enumerate(self) if condition(person)]
         subpop = NumpySubpopulationProxy(self._person_store, self._at_t, subpop_indices)
         return subpop
 
