58441bd26305079547c77364879573967771b1a6
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-10-05 17:59:36 -0400

    fixup PopulationIterator

--- microsim/store/numpy_population_iterator.py
+++ microsim/store/numpy_population_iterator.py
@@ -2,16 +2,14 @@ class NumpyPopulationIterator:
     """Iterator for a whole population without conditions."""
 
     def __init__(self, person_store, at_t):
-        self._store = person_store
+        self._person_store = person_store
         self._at_t = at_t
-        self._range = range(self._num_persons)
+        self._range = iter(range(self._person_store._num_persons))
 
     def __iter__(self):
         return self
 
     def __next__(self):
         abs_person_idx = next(self._range)
-        person_proxy = self._person_store.get_person_proxy_at(
-            abs_person_idx, self._at_t, scratch_next=self._scratch_next
-        )
+        person_proxy = self._person_store.get_person_proxy_at(abs_person_idx, self._at_t)
         return person_proxy
