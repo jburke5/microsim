9dd8e62c5e099bbb4ee4eb678750351cdefc54b4
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-10-11 16:06:31 -0400

    update with_scratch_next docstring

--- microsim/store/numpy_subpopulation_proxy.py
+++ microsim/store/numpy_subpopulation_proxy.py
@@ -42,7 +42,12 @@ class NumpySubpopulationProxy:
         return person_proxy
 
     def with_scratch_next(self):
-        """Returns subpopulation with same members but with scratch `.next`."""
+        """
+        Returns a subpopulation with the same members but with scratch `.next`.
+
+        Intended for hypothetical/counterfactual scenarios and/or baseline comparisons. Data stored
+        in scratch `.next` should not be expected to persist between ticks.
+        """
         return NumpySubpopulationProxy(
             self._person_store, self._at_t, self._member_indices, scratch_next=True
         )
