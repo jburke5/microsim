b708c58bf7ed50838f7c7833d062bd6c6d9ec5a9
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-11-04 15:30:49 -0400

    add first non-deterministic model test

---
+++ microsim/test/_validation/test_non_deterministic_models.py
@@ -0,0 +1,19 @@
+"""Confirm model results do not vary significantly across Person representations."""
+
+import numpy as np
+from microsim.test._validation.fixture import StorePopulationValidationFixture
+
+
+class TestNonDeterministicModels(StorePopulationValidationFixture):
+    def test_physical_activity_model(self):
+        model = self._risk_model_repository.get_model("anyPhysicalActivity")
+        vec_df = self.vec_pop.get_people_current_state_and_summary_as_dataframe()
+        cur_pop = self.store_pop.person_store.get_population_at(0)
+
+        for (_, vec_row), store_person in zip(vec_df.iterrows(), cur_pop):
+            model_random_state = np.random.get_state()
+            vec_model_result = model.estimate_next_risk_vectorized(vec_row)
+            np.random.set_state(model_random_state)
+            store_model_result = model.estimate_next_risk_vectorized(store_person)
+
+            self.assertEqual(vec_model_result, store_model_result)
