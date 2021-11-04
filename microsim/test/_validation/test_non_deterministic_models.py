1507d0c0970b44182da6638b50b3dcbcb26b0ec4
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2021-11-04 16:40:04 -0400

    add tests for other nondet. cohort risk models

--- microsim/test/_validation/test_non_deterministic_models.py
+++ microsim/test/_validation/test_non_deterministic_models.py
@@ -12,8 +12,47 @@ class TestNonDeterministicModels(StorePopulationValidationFixture):
         self._random_seed = 60632844
         np.random.seed(self._random_seed)
 
+    def test_cohort_afib_model(self):
+        model = self._cohort_risk_model_repository.get_model("afib")
+        vec_df = self.vec_pop.get_people_current_state_and_summary_as_dataframe()
+        cur_pop = self.store_pop.person_store.get_population_at(0)
+
+        for (_, vec_row), store_person in zip(vec_df.iterrows(), cur_pop):
+            model_random_state = np.random.get_state()
+            vec_model_result = model.estimate_next_risk_vectorized(vec_row)
+            np.random.set_state(model_random_state)
+            store_model_result = model.estimate_next_risk_vectorized(store_person)
+
+            self.assertEqual(vec_model_result, store_model_result)
+
+    def test_cohort_antihypertensive_count_model(self):
+        model = self._cohort_risk_model_repository.get_model("antiHypertensiveCount")
+        vec_df = self.vec_pop.get_people_current_state_and_summary_as_dataframe()
+        cur_pop = self.store_pop.person_store.get_population_at(0)
+
+        for (_, vec_row), store_person in zip(vec_df.iterrows(), cur_pop):
+            model_random_state = np.random.get_state()
+            vec_model_result = model.estimate_next_risk_vectorized(vec_row)
+            np.random.set_state(model_random_state)
+            store_model_result = model.estimate_next_risk_vectorized(store_person)
+
+            self.assertEqual(vec_model_result, store_model_result)
+
     def test_cohort_physical_activity_model(self):
         model = self._cohort_risk_model_repository.get_model("anyPhysicalActivity")
+        vec_df = self.vec_pop.get_people_current_state_and_summary_as_dataframe()
+        cur_pop = self.store_pop.person_store.get_population_at(0)
+
+        for (_, vec_row), store_person in zip(vec_df.iterrows(), cur_pop):
+            model_random_state = np.random.get_state()
+            vec_model_result = model.estimate_next_risk_vectorized(vec_row)
+            np.random.set_state(model_random_state)
+            store_model_result = model.estimate_next_risk_vectorized(store_person)
+
+            self.assertEqual(vec_model_result, store_model_result)
+
+    def test_cohort_statin_model(self):
+        model = self._cohort_risk_model_repository.get_model("statin")
         vec_df = self.vec_pop.get_people_current_state_and_summary_as_dataframe()
         cur_pop = self.store_pop.person_store.get_population_at(0)
 
