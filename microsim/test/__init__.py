654dc2c0da3b8562eb3a75cddfa5def7d4799529
Luciana Copeland <45376498+llc2@users.noreply.github.com>
2022-02-22 15:08:11 -0500

    add run_all_tests function to microsim.test

--- microsim/test/__init__.py
+++ microsim/test/__init__.py
@@ -1,7 +1,6 @@
 import os
-
-
-__all__ = []
+import sys
+import unittest
 
 
 def load_tests(loader, standard_tests, pattern):
@@ -11,3 +10,11 @@ def load_tests(loader, standard_tests, pattern):
     package_tests = loader.discover(test_dir, discover_pattern)
     standard_tests.addTests(package_tests)
     return standard_tests
+
+
+def run_all_tests():
+    test_module = sys.modules[__name__]
+    unittest.main(module=test_module)
+
+
+__all__ = [run_all_tests]
