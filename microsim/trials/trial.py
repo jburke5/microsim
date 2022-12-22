d665ea08caac8c96df718f64a527cf777c40b1a6
Christos Deligkaris <deligkaris@users.noreply.github.com>
2022-12-22 16:38:49 -0500

    added support for parallel trialsets

--- microsim/trials/trial.py
+++ microsim/trials/trial.py
@@ -5,17 +5,18 @@ import copy
 import pandas as pd
 
 class Trial:
-    def __init__(self, trialDescription, targetPopulation):
+    
+    def __init__(self, trialDescription, targetPopulation, additionalLabels=None): 
         self.trialDescription = trialDescription
-        self.trialPopulation = self.select_trial_population(targetPopulation, 
-            trialDescription.inclusionFilter, trialDescription.exclusionFilter)
+        self.trialPopulation = self.select_trial_population(targetPopulation, trialDescription.inclusionFilter, trialDescription.exclusionFilter)
         # select our patients from the population
         self.maxSampleSize = pd.Series(trialDescription.sampleSizes).max()
         self.treatedPop, self.untreatedPop = self.randomize(trialDescription.randomizationSchema)
         self.analyticResults = {}
+        self.additionalLabels = additionalLabels 
 
     def select_trial_population(self, targetPopulation, inclusionFilter, exclusionFilter):
-        filteredPeople = list(filter(inclusionFilter, list(targetPopulation._people)))
+        filteredPeople = list(filter(inclusionFilter, list(targetPopulation._people))) 
         return PersonListPopulation(filteredPeople)
 
     def randomize(self, randomizationSchema):
@@ -24,7 +25,7 @@ class Trial:
         randomizedCount = 0
         # might be able to make this more efficient by sampling from the filtered people...
         for i, person in self.trialPopulation._people.items():
-            while randomizedCount < self.maxSampleSize: 
+            while randomizedCount < self.maxSampleSize:
                 if not person.is_dead():
                     if randomizationSchema(person):
                         treatedList.append(copy.deepcopy(person))
@@ -32,18 +33,20 @@ class Trial:
                         untreatedList.append(copy.deepcopy(person))
                     randomizedCount+=1
                 else:
-                    continue 
+                    continue
         return PersonListPopulation(treatedList), PersonListPopulation(untreatedList)
-    
+
     def run(self):
         self.treatedPop._bpTreatmentStrategy = self.trialDescription.treatment
         lastDuration = 0
         for duration in self.trialDescription.durations:
             self.treatedDF, self.treatedAlive = self.treatedPop.advance_vectorized(duration-lastDuration)
             self.untreatedDF, self.untreatedAlive = self.untreatedPop.advance_vectorized(duration-lastDuration)
-            self.analyze(duration, self.maxSampleSize, self.treatedPop._people.tolist(), self.untreatedPop._people.tolist())
+            self.analyze(duration, 
+                         self.maxSampleSize, 
+                         self.treatedPop._people.tolist(), 
+                         self.untreatedPop._people.tolist())
             self.analyzeSmallerTrials(duration)
-            
             lastDuration = duration
 
     def analyzeSmallerTrials(self, duration):
@@ -56,7 +59,6 @@ class Trial:
                 sampleUntreated = self.untreatedPop._people.sample(int(sampleSize/2))
                 self.analyze(duration, sampleSize, sampleTreated.tolist(), sampleUntreated.tolist())
 
-
     def analyze(self, duration, sampleSize, treatedPopList, untreatedPopList):
         for analysis in self.trialDescription.analyses:
             reg, se, pvalue, absoluteEffectSize = None, None, None, None
@@ -64,16 +66,14 @@ class Trial:
                 reg, se, pvalue, absoluteEffectSize = analysis.analyze(treatedPopList, untreatedPopList)
             except PerfectSeparationError: # how to track these is not obvious, now now we'll enter "Nones"
                 pass
-            self.analyticResults[get_analysis_name(analysis, duration, sampleSize)] = {'reg' : reg, 
-                                                                                        'se' : se, 
-                                                                                        'pvalue': pvalue,
-                                                                                        'absEffectSize' : absoluteEffectSize,
-                                                                                        'duration' : duration, 
-                                                                                        'sampleSize' : sampleSize,
-                                                                                        'outcome' :  analysis.outcomeAssessor.get_name(),
-                                                                                        'analysis' : analysis.name}
+            self.analyticResults[get_analysis_name(analysis, duration, sampleSize)] = {  'reg' : reg,
+                                                                                         'se' : se,
+                                                                                         'pvalue': pvalue,
+                                                                                         'absEffectSize' : absoluteEffectSize,
+                                                                                         'duration' : duration,
+                                                                                         'sampleSize' : sampleSize,
+                                                                                         'outcome' :  analysis.outcomeAssessor.get_name(),
+                                                                                         'analysis' : analysis.name}
         return self.analyticResults
-        
-        
 
-    
+
--- microsim/trials/trial_utils.py
+++ microsim/trials/trial_utils.py
@@ -1,2 +1,9 @@
+import numpy as np
+
 def get_analysis_name(analysis, duration, sampleSize):
     return f"{analysis.name}-{str(duration)}Years-{sampleSize}"
+
+#this function can be generalized, for more distributions
+def randomizationSchema(x):
+    return (np.random.uniform() < 0.5) #assumes a uniform distribution
+
