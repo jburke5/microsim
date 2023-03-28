9996b7c57e564da95f4d8fad197d7da17da910c1
Jim Burke <jburke5@gmail.com>
2023-03-28 09:52:27 -0400

    adding cox_regerssion_analysis class

---
+++ microsim/trials/cox_regression_analysis.py
@@ -0,0 +1,20 @@
+from lifelines import CoxPHFitter
+from numpy.linalg import LinAlgError
+import numpy as np
+from microsim.trials.regression_analysis import RegressionAnalysis
+
+class CoxRegressionAnalysis(RegressionAnalysis):
+    def __init__(self, outcomeAssessor, nameStem='coxRegression-'):
+        super().__init__(outcomeAssessor, nameStem)
+
+    def analyze(self, treatedPop, untreatedPop):
+        data=self.get_dataframe(treatedPop,untreatedPop,includeTime=True)
+        cph = CoxPHFitter()
+        try:
+            cph.fit(data, duration_col='time', event_col='outcome')
+
+            return cph.params_['treatment'], None, cph.standard_errors_['treatment'], cph.summary.loc['treatment', 'p'], self.get_means(data)
+        except LinAlgError:
+            return np.nan, np.nan, np.nap, np.nan, np.nan, np.nan
+
+
