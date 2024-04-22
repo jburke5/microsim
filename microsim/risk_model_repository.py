cbdff6999810faec7608c843227e01aa054969b8
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-04-22 09:25:54 -0400

    updated lower and upper bounds in risk_model_repository

--- microsim/risk_model_repository.py
+++ microsim/risk_model_repository.py
@@ -7,34 +7,30 @@ from microsim.risk_factor import DynamicRiskFactorsType
 class RiskModelRepository:
     def __init__(self):
         self._repository = {}
-        #self._lowerBounds = {DynamicRiskFactorsType.SBP.value: 60,
-        #                     DynamicRiskFactorsType.DBP.value: 20,
-        #                     DynamicRiskFactorsType.CREATININE.value: 0.1}
-        #self._upperBounds = {DynamicRiskFactorsType.SBP.value: 300,
-        #                     DynamicRiskFactorsType.DBP.value: 180}
-        #bounds based on NHANES data from 1999
-        self._lowerBounds = {DynamicRiskFactorsType.SBP.value: 72.67,
-                             DynamicRiskFactorsType.DBP.value: 40.00,
-                             DynamicRiskFactorsType.CREATININE.value: 0.20,
-                             DynamicRiskFactorsType.WAIST.value: 58.50,
-                             DynamicRiskFactorsType.LDL.value: 28.00,
-                             DynamicRiskFactorsType.A1C.value: 2.50,
-                             DynamicRiskFactorsType.TRIG.value: 12.00,
-                             DynamicRiskFactorsType.BMI.value: 12.04,
-                             DynamicRiskFactorsType.HDL.value: 8.00,
+        #bounds based on NHANES data from 1999 to 20017 (all data), 0.9*nhanesMin, 1.1*nhanesMax
+        #age is an exception, bounds set manually
+        self._lowerBounds = {DynamicRiskFactorsType.SBP.value: 58.20,
+                             DynamicRiskFactorsType.DBP.value: 36. ,
+                             DynamicRiskFactorsType.CREATININE.value: 0.090,
+                             DynamicRiskFactorsType.WAIST.value: 49.95,
+                             DynamicRiskFactorsType.LDL.value: 8.10,
+                             DynamicRiskFactorsType.A1C.value: 1.80,
+                             DynamicRiskFactorsType.TRIG.value: 9.,
+                             DynamicRiskFactorsType.BMI.value: 10.836,
+                             DynamicRiskFactorsType.HDL.value: 5.4,
                              DynamicRiskFactorsType.AGE.value: 18,
-                             DynamicRiskFactorsType.TOT_CHOL.value: 72.00}
-        self._upperBounds = {DynamicRiskFactorsType.SBP.value: 266.00,
-                             DynamicRiskFactorsType.DBP.value: 132.00,
-                             DynamicRiskFactorsType.CREATININE.value: 11.80,
-                             DynamicRiskFactorsType.WAIST.value: 173.40,
-                             DynamicRiskFactorsType.LDL.value: 354.00,
-                             DynamicRiskFactorsType.A1C.value: 15.10,
-                             DynamicRiskFactorsType.TRIG.value: 2141.00,
-                             DynamicRiskFactorsType.BMI.value: 67.83,
-                             DynamicRiskFactorsType.HDL.value: 151.00,
-                             DynamicRiskFactorsType.AGE.value: 85,
-                             DynamicRiskFactorsType.TOT_CHOL.value: 525.00}
+                             DynamicRiskFactorsType.TOT_CHOL.value: 53.1}
+        self._upperBounds = {DynamicRiskFactorsType.SBP.value: 297.,
+                             DynamicRiskFactorsType.DBP.value: 152.53,
+                             DynamicRiskFactorsType.CREATININE.value: 19.58,
+                             DynamicRiskFactorsType.WAIST.value: 196.9,
+                             DynamicRiskFactorsType.LDL.value: 691.9,
+                             DynamicRiskFactorsType.A1C.value: 20.68,
+                             DynamicRiskFactorsType.TRIG.value: 4656.3,
+                             DynamicRiskFactorsType.BMI.value: 143.23,
+                             DynamicRiskFactorsType.HDL.value: 248.6,
+                             DynamicRiskFactorsType.AGE.value: 130,
+                             DynamicRiskFactorsType.TOT_CHOL.value: 894.3}
 
     def apply_bounds(self, varName, varValue):
         """
