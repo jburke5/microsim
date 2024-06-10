317aa58c5c0a32c1600598df19794fa9acc87b3e
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-06-10 15:31:32 -0400

    updated bp treatment strategies

--- microsim/bp_treatment_strategies.py
+++ microsim/bp_treatment_strategies.py
@@ -12,15 +12,14 @@ from abc import ABC
 # combined with the number added, additionalliy, via treatment algorithms
 class BaseTreatmentStrategy:
     MAX_BP_MEDS = 4
-
-    def get_changes_vectorized(self, x):
-        raise RuntimeError("Vectorized Treatment not implemented for this strategy")
+    SBP_MULTIPLIER = 5.5
+    DBP_MULTIPLIER = 3.1
 
 class AddNBPMedsTreatmentStrategy(BaseTreatmentStrategy):
     def __init__(self, n):
         self.bpMedsAdded = n
-        self.sbpLowering = 5.5*self.bpMedsAdded
-        self.dbpLowering = 3.1*self.bpMedsAdded
+        self.sbpLowering = self.SBP_MULTIPLIER * self.bpMedsAdded
+        self.dbpLowering = self.DBP_MULTIPLIER * self.bpMedsAdded
         self.status = TreatmentStrategyStatus.BEGIN
 
     def get_updated_treatments(self, person):
@@ -38,58 +37,16 @@ class AddNBPMedsTreatmentStrategy(BaseTreatmentStrategy):
         else:
             return dict()
 
-class AddASingleBPMedTreatmentStrategy(BaseTreatmentStrategy):
-    sbpLowering = 5.5
-    dbpLowering = 3.1
-    bpMedsAdded = 1
+class AddASingleBPMedTreatmentStrategy(AddNBPMedsTreatmentStrategy):
+    sbpLowering = BaseTreatmentStrategy.SBP_MULTIPLIER
+    dbpLowering = BaseTreatmentStrategy.DBP_MULTIPLIER
 
     def __init__(self):
-        self.status = TreatmentStrategyStatus.BEGIN
-
-    def get_updated_treatments(self, person):
-        return dict()
-
-    def get_updated_risk_factors(self, person):
-        if person._treatmentStrategies[TreatmentStrategiesType.BP.value]["status"]==TreatmentStrategyStatus.BEGIN:
-            person._treatmentStrategies[TreatmentStrategiesType.BP.value]["bpMedsAdded"]=self.bpMedsAdded
-            return {DynamicRiskFactorsType.SBP.value: getattr(person, "_"+DynamicRiskFactorsType.SBP.value)[-1] - self.sbpLowering,
-                    DynamicRiskFactorsType.DBP.value: getattr(person, "_"+DynamicRiskFactorsType.DBP.value)[-1] - self.dbpLowering}
-        elif person._treatmentStrategies[TreatmentStrategiesType.BP.value]["status"]==TreatmentStrategyStatus.END:
-            del person._treatmentStrategies[TreatmentStrategiesType.BP.value]["bpMedsAdded"]
-            return {DynamicRiskFactorsType.SBP.value: getattr(person, "_"+DynamicRiskFactorsType.SBP.value)[-1] + self.sbpLowering,
-                    DynamicRiskFactorsType.DBP.value: getattr(person, "_"+DynamicRiskFactorsType.DBP.value)[-1] + self.dbpLowering}
-        else:
-            return dict()
-
-    #def get_changes_for_person(self, person):
-    #    return (
-    #        {"_antiHypertensiveCount": 1},
-    #        {"_bpMedsAdded": 1},
-    #        {
-    #            "_sbp": -1 * AddASingleBPMedTreatmentStrategy.sbpLowering,
-    #            "_dbp": -1 * AddASingleBPMedTreatmentStrategy.dbpLowering,
-    #        },
-    #    )
-
-    #def get_treatment_recalibration_for_population(self):
-    #    return {OutcomeType.STROKE: 0.79, OutcomeType.MI: 0.87}
-
-    #def get_treatment_recalibration_for_person(self):
-    #    return {OutcomeType.STROKE: 0.79, OutcomeType.MI: 0.87}
-
-    #def repeat_treatment_strategy(self):
-    #    return False
-
-    #def get_changes_vectorized(self, x):
-    #    x.bpMedsAddedNext = 1
-    #    x.totalBPMedsAddedNext = 1
-    #    x.sbpNext = x.sbpNext - AddASingleBPMedTreatmentStrategy.sbpLowering
-    #    x.dbpNext = x.dbpNext - AddASingleBPMedTreatmentStrategy.dbpLowering
-    #    return x
+        super().__init__(1)
 
 class AddBPTreatmentMedsToGoal120(BaseTreatmentStrategy):
-    sbpLowering = 5.5
-    dbpLowering = 3.1
+    sbpLowering = BaseTreatmentStrategy.SBP_MULTIPLIER
+    dbpLowering = BaseTreatmentStrategy.DBP_MULTIPLIER
 
     def __init__(self):
         self.status = TreatmentStrategyStatus.BEGIN
@@ -127,46 +84,6 @@ class AddBPTreatmentMedsToGoal120(BaseTreatmentStrategy):
         medCount = dbpMedCount if dbpMedCount < sbpMedCount else sbpMedCount
         return 0 if medCount < 0 else int(medCount)
 
-    #def get_changes_for_person(self, person):        
-    #    medsForGoal = self.get_meds_needed_for_goal(
-    #        person._sbp[-1],
-    #        person._dbp[-1],
-    #        person._antiHypertensiveCount[-1] + person._bpMedsAdded[-1],
-    #        self.getGoal(person),
-    #    )
-    #    return (
-    #        {},
-    #        {"_bpMedsAdded": medsForGoal},
-    #        {
-    #            "_sbp": -1 * medsForGoal * AddBPTreatmentMedsToGoal120.sbpLowering,
-    #            "_dbp": -1 * medsForGoal * AddBPTreatmentMedsToGoal120.dbpLowering,
-    #        },
-    #    )
-
-    #def get_treatment_recalibration_for_population(self):
-    #    return {OutcomeType.STROKE: 0.79, OutcomeType.MI: 0.87}
-
-    #def get_treatment_recalibration_for_person(self, person):
-    #    medsForGoal = self.get_meds_needed_for_goal(person.sbp[-1], person.dbp[-1])
-    #    return {OutcomeType.STROKE: 0.79**medsForGoal, OutcomeType.MI: 0.87**medsForGoal}
-
-    #def repeat_treatment_strategy(self):
-    #    return True
-
-    #def get_goal_vectorized(self, x):
-    #    return self.getGoal()
-
-    #def get_changes_vectorized(self, x):
-    #    medsNeeded = self.get_meds_needed_for_goal(
-    #        x.sbpNext, x.dbpNext, x.antiHypertensiveCountNext + x.totalBPMedsAdded, self.get_goal_vectorized(x)
-    #    )
-    #    # how many medications to add in addition to baseline
-    #    x.bpMedsAddedNext =  medsNeeded 
-    #    x.totalBPMedsAddedNext = x.totalBPMedsAdded + medsNeeded
-    #    x.sbpNext = x.sbpNext - medsNeeded * AddBPTreatmentMedsToGoal120.sbpLowering
-    #    x.dbpNext = x.dbpNext - medsNeeded * AddBPTreatmentMedsToGoal120.dbpLowering
-    #    return x
-
 class NoBPTreatment(BaseTreatmentStrategy):
     def __init__(self):
         self.status = TreatmentStrategyStatus.BEGIN
@@ -177,29 +94,6 @@ class NoBPTreatment(BaseTreatmentStrategy):
     def get_updated_risk_factors(self, person):
         return dict()
 
-    #def get_changes_for_person(self, person):
-    #    current = person._antiHypertensiveCount[-1]
-    #    return (
-    #        {"_antiHypertensiveCount": 0},
-    #        {"_bpMedsAdded": 0},
-    #        {"_sbp": 0, "_dbp": 0},
-    #    )
-
-    #def get_treatment_recalibration_for_population(self):
-    #    return None
-
-    #def get_treatment_recalibration_for_person(self):
-    #    return None
-
-    #def repeat_treatment_strategy(self):
-    #    return True
-
-    # BP meds can change via the usual care risk model...but, we won't add any meds...
-    #def get_changes_vectorized(self, x):
-    #    x.bpMedsAddedNext = 0
-    #    x.totalBPMedsAddedNext = 0
-    #    return x
-
 # James, P. A. et al. 2014 Evidence-Based Guideline for the Management of High Blood Pressure in Adults: Report From the Panel Members Appointed to the Eighth Joint National Committee (JNC 8). Jama 311, 507–520 (2014).
 class jnc8Treatment(AddBPTreatmentMedsToGoal120):
     def low_target(self, person):
@@ -208,13 +102,6 @@ class jnc8Treatment(AddBPTreatmentMedsToGoal120):
     def get_goal(self, person):
         return {'sbp' : 140, 'dbp' : 90} if self.low_target(person) else {'sbp' : 150, 'dbp' : 90}
         
-    #def lowTargetVectorized(self, x):
-    #    #print(list(x.index))
-    #    return x.current_diabetes or x.gfr < 60 or x.age < 60
-        
-    #def get_goal_vectorized(self, x):
-    #    return {'sbp' : 140, 'dbp' : 90} if self.lowTargetVectorized(x) else {'sbp' : 150, 'dbp' : 90}
-
     def get_meds_needed_for_goal(self, person, goal):
         meds = super().get_meds_needed_for_goal(person, goal)
         currentMeds = person._antiHypertensiveCountPlusBPMedsAdded()
@@ -232,10 +119,6 @@ class jnc8ForHighRisk(jnc8Treatment):
         risk = self.cvModelRepository.select_outcome_model_for_person(person).get_risk_for_person(person, years=10)
         return risk >  self.targetRisk
     
-    #def lowTargetVectorized(self, x):
-    #    risk = OutcomeModelRepository().get_risk_for_person(x, OutcomeModelType.CARDIOVASCULAR, 10, True)
-    #    return risk >  self.targetRisk
-
 class jnc8ForHighRiskLowBpTarget(jnc8ForHighRisk):
     def __init__(self, targetRisk, targetBP):
         super().__init__(targetRisk)
@@ -245,9 +128,6 @@ class jnc8ForHighRiskLowBpTarget(jnc8ForHighRisk):
     def get_goal(self, person):
         return self.targetBP 
     
-    #def get_goal_vectorized(self, x):
-    #    return self.targetBP
-
 # simplified class to represent SPRINT.
 class SprintTreatment(jnc8ForHighRiskLowBpTarget):
     def __init__(self):
